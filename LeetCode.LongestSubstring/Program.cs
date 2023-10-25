a7923b68c5634a1af4af9e1fe7ef65291755f1f2
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-10-25 19:48:48 +0300

    .

---
+++ LeetCode.LongestSubstring/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.LongestSubstring
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var value = solution.LengthOfLongestSubstring("aqwertyuio");
+            Console.WriteLine(value);
+        }
+    }
+
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            char[] chars = new char[s.Length];
+            char a = s[0];
+            int i = 1;
+            while (a != s[i] && i< s.Length -1)
+            {
+                    i++;
+            }
+            
+
+
+            return i;
+        }
+    }
+}
