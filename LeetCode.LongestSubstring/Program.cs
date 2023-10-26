0f7d0be5c234eab440d62da67d9d44731de2ee0b
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-10-26 15:18:53 +0300

    .

--- LeetCode.LongestSubstring/Program.cs
+++ LeetCode.LongestSubstring/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.LongestSubstring
 {
@@ -7,8 +9,9 @@ namespace LeetCode.LongestSubstring
         public static void Main(string[] args)
         {
             Solution solution = new Solution();
-            var value = solution.LengthOfLongestSubstring("aqwertyuio");
-            Console.WriteLine(value);
+            int a = solution.LengthOfLongestSubstring("asdasd");
+            Console.WriteLine(a);
+
         }
     }
 
@@ -16,17 +19,75 @@ namespace LeetCode.LongestSubstring
     {
         public int LengthOfLongestSubstring(string s)
         {
-            char[] chars = new char[s.Length];
-            char a = s[0];
-            int i = 1;
-            while (a != s[i] && i< s.Length -1)
+            var aPointer = 0;
+            var bPointer = 0;
+            var max = 0;
+
+            var hashSet = new HashSet<char>();
+
+            while (bPointer < s.Length)
             {
-                    i++;
+                if (!hashSet.Contains(s[bPointer]))
+                {
+                    hashSet.Add(s[bPointer]);
+                    bPointer++;
+                    max = Math.Max(hashSet.Count, max);
+                }
+                else
+                {
+                    hashSet.Remove(s[aPointer]);
+                    aPointer++;
+                }
             }
-            
 
+            return max;
 
-            return i;
         }
+        
+        
+        // public int LengthOfLongestSubstring(string s)
+        // {
+        //     char[] chars = new char[s.Length];
+        //     int i = 0;
+        //     while (i < s.Length)
+        //     {
+        //         if (i != 0)
+        //         {
+        //             int dubliceIndex = Dublicate(chars,s[i]);
+        //             if (dubliceIndex != -1)
+        //             {
+        //                 chars[dubliceIndex] = ' ';
+        //             }
+        //         }
+        //         chars[i] = s[i];
+        //         i++;
+        //     }
+        //
+        //     for (int j = 0; j < chars.Length; j++)
+        //     {
+        //         if (chars[j] != ' ')
+        //         {
+        //             Console.Write($"{chars[j]}-");
+        //         }
+        //     }
+        //     return 0;
+        // }
+        // private int Dublicate(char[] chars, char c)
+        // {
+        //     if (chars.Contains(c))
+        //     {
+        //         for (int i = 0; i < chars.Length; i++)
+        //         {
+        //             if (chars[i] == c)
+        //             {
+        //                 return i;
+        //             }
+        //         }
+        //     }
+        //
+        //     return -1;
+        // }
     }
+    
+
 }
