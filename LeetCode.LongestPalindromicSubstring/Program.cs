e37af5bd9afb172766ab083ab3c675f93c5f803c
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-10-26 19:24:30 +0300

    .

---
+++ LeetCode.LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.LongestPalindromicSubstring
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.LongestPalindrome("kabbal");
+        }
+    }
+
+    public class Solution
+    {
+        public string LongestPalindrome(string s)
+        {
+            
+        }
+
+        
+    }
+}
