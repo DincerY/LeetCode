8369203774dc76aa30b1f4c0f7c538b8846f187e
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-10-27 09:52:31 +0300

    Create Zigzag

--- LeetCode.ZigzagConversion/Program.cs
+++ LeetCode.ZigzagConversion/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace LeetCode.ZigzagConversion
+﻿using System;
+
+namespace LeetCode.ZigzagConversion
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Deneme deneme = new Deneme();
+            deneme.CreateZigzag();
+            
+
+
+        }
+
+        class Deneme
+        {
+            public void CreateZigzag()
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write("*");
+
+                    for (int j = 5 - i - 1; j > 0; j--)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    Console.Write("*");
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
