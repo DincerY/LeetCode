3174b56fa0f5e767cc217ef0551d4e80c9999b44
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-11-17 16:55:23 +0300

    .dd

---
+++ LeetCode.FindTheIndexOf/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCode.FindTheIndexOf
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+            solution.StrStr("mississippi","sippi");
+        }
+    }
+    
+    public class Solution {
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack.Length < needle.Length)
+            {
+                return -1;
+            }
+            int l = 0;
+            for (int r = 0; r < haystack.Length; r++)
+            {
+                if (r+needle.Length>haystack.Length)
+                {
+                    return -1;
+                }
+                if (haystack[r] == needle[0])
+                {
+                    for (int i = r; i < r + needle.Length; i++)
+                    {
+                        if (haystack[i] == needle[l])
+                        {
+                            l++;
+                        }
+                        else
+                        {
+                            l = 0;
+                            break;
+                        }
+                        if (l == needle.Length)
+                        {
+                            return r;
+                        }
+                    }
+                }
+              
+                
+           
+            }
+
+            if (l == 0)
+            {
+                return -1;
+            }
+            return l;
+
+
+        }
+    }
+}
---
+++ LeetCode.RemoveElement/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.RemoveElement
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+            solution.RemoveElement(new[] { 0,1,2,2,3,0,4,2 }, 2);
+        }
+    }
+
+
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int l = 0;
+            for (int r = 0; r < nums.Length; r++)
+            {
+                if (nums[r] != val)
+                {
+                    nums[l] = nums[r];
+                    l++;
+                }
+            }
+            return l;
+        }
+    }
+}
