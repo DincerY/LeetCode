c6162681fbd40e35c664936afbb7b1f98692509b
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-11-17 18:57:59 +0300

    Accept

--- LeetCode.FindTheIndexOf/Program.cs
+++ LeetCode.FindTheIndexOf/Program.cs
@@ -1,28 +1,34 @@
-﻿namespace LeetCode.FindTheIndexOf
+﻿using System.Linq;
+
+namespace LeetCode.FindTheIndexOf
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
             Solution solution = new();
-            solution.StrStr("mississippi","sippi");
+            // solution.StrStr("mississippi", "sippi");
+            solution.Str("mississippi", "sippi");
         }
     }
-    
-    public class Solution {
+
+    public class Solution
+    {
         public int StrStr(string haystack, string needle)
         {
             if (haystack.Length < needle.Length)
             {
                 return -1;
             }
+
             int l = 0;
             for (int r = 0; r < haystack.Length; r++)
             {
-                if (r+needle.Length>haystack.Length)
+                if (r + needle.Length > haystack.Length)
                 {
                     return -1;
                 }
+
                 if (haystack[r] == needle[0])
                 {
                     for (int i = r; i < r + needle.Length; i++)
@@ -36,6 +42,7 @@
                             l = 0;
                             break;
                         }
+
                         if (l == needle.Length)
                         {
                             return r;
@@ -43,11 +50,72 @@
                     }
                 }
             }
+
             if (l == 0)
             {
                 return -1;
             }
+
             return l;
+        }
+
+
+        public int Str(string haystack, string needle)
+        {
+            if (haystack.Length < needle.Length)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                string result = "";
+                if (i + needle.Length > haystack.Length)
+                {
+                    return -1;
+                }
+                if (haystack[i] == needle[0])
+                {
+                    for (int j = 0; j < needle.Length; j++)
+                    {
+                        result += haystack[i+j];
+                    }
+
+                }
+                if (result == needle)
+                {
+                    return i;
+                }
+            }
+            
+            return 0;
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
