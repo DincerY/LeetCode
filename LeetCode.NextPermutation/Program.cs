703f9fdf677a3ee109cc36ff95e30c53820cc2f0
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-11-24 14:41:34 +0300

    Accept

---
+++ LeetCode.NextPermutation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.NextPermutation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+            solution.NextPermutation(new[] { 1,1 });
+        }
+    }
+
+
+    public class Solution
+    {
+        public void NextPermutation(int[] nums)
+        {
+            int i = nums.Length - 2;
+            while (i >= 0 && nums[i] >= nums[i + 1])
+            {
+                i--;
+            }
+
+            if (i>=0)
+            {
+                int j = nums.Length - 1;
+
+                while (nums[j] <= nums[i])
+                {
+                    j--;
+                }
+                (nums[i], nums[j]) = (nums[j], nums[i]);
+            }
+            
+
+            nums.AsSpan(i+1, nums.Length - i-1).Reverse();
+        }
+
+    }
+}
---
+++ LeetCode.SubstringWithConcatenation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.SubstringWithConcatenation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+            solution.FindSubstring("", new[] { "ab", "cd","ef"});
+        }
+    }
+
+    public class Solution
+    {
+        public List<string> Combination { get; set; }
+
+        public Solution()
+        {
+            Combination = new List<string>();
+        }
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            int length = words.Length * (words.Length - 1);
+            
+            s.Contains("");
+            CombinateWords(words);
+
+            Console.WriteLine(Combination);
+            
+            
+            
+            return null;
+        }
+
+        private void CombinateWords(string[] words)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                var deneme = words[i];
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (j==i)
+                    {
+                        continue;
+                    }
+
+                    deneme += words[j];
+                }
+                Combination.Add(deneme);
+            }
+        }
+    }
+}
+
