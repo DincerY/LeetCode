9a21fd0980316b02e193e61b620884e899227504
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-01 18:23:46 +0300

    ..

---
+++ LeetCode.CombinationSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.CombinationSum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new();
+            solution.Recursive(1);
+        }
+    }
+    
+    
+    public class Solution {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+
+           
+
+
+
+
+            return null;
+        }
+
+        public void Recursive(int[] candidates,int a)
+        {
+            if (a < 5)
+            {
+                Console.WriteLine(a);
+                Recursive(candidates,a+1);
+                Recursive(candidates,a+1);
+            }
+        }
+        
+    }
+}
