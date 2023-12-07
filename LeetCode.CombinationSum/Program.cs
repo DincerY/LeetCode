6e2ce1af3a0a516d6f1ccd463cfe23f361073486
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-07 09:35:07 +0300

    Accept

--- LeetCode.CombinationSum/Program.cs
+++ LeetCode.CombinationSum/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
 
 namespace LeetCode.CombinationSum
 {
@@ -8,32 +10,44 @@ namespace LeetCode.CombinationSum
         public static void Main(string[] args)
         {
             Solution solution = new();
-            solution.Recursive(new []{1,2,3},1);
+            solution.CombinationSum(new []{2,3,5},8);
+
         }
     }
-    
-    
-    public class Solution {
+
+
+    public class Solution
+    {
         public IList<IList<int>> CombinationSum(int[] candidates, int target)
         {
+            List<IList<int>> res = new List<IList<int>>();
 
-           
-
-
-
-
-            return null;
-        }
-
-        public void Recursive(int[] candidates,int a)
-        {
-            if (a < 5)
+            void Recursive(int i,List<int> cur,int total)
             {
-                Console.WriteLine(a);
-                Recursive(candidates,a+1);
-                Recursive(candidates,a+1);
+                if (total == target)
+                {
+                    res.Add(new List<int>(cur));
+                    return;
+                }
+                
+                if (i < candidates.Length)
+                {
+                    if (total + candidates[i] <= target)
+                    {
+                        cur.Add(candidates[i]);
+                        Recursive(i,cur,total+candidates[i]);
+                        cur.RemoveAt(cur.Count-1);
+                    }
+                    Recursive(i+1,cur,total);
+                }
             }
+
+            Recursive(0,new List<int>(),0);
+
+            return res;
         }
+
+        
         
     }
 }
