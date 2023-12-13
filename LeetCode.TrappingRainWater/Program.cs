e4be1379ce6baa65514a4c3540ed620531a567f6
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-13 17:18:58 +0300

    Accept

--- LeetCode.TrappingRainWater/Program.cs
+++ LeetCode.TrappingRainWater/Program.cs
@@ -9,7 +9,8 @@ namespace LeetCode.TrappingRainWater
         public static void Main(string[] args)
         {
             Solution solution = new();
-            solution.TrapWithOneSpace(new []{4,2,0,3,2,5});
+            //solution.TrapWithOneSpace(new []{4,2,0,3,2,5});
+            solution.Trap(new[] { 0,1,0,2,1,0,1,3,2,1,2,1 });
 
 
         }
@@ -60,14 +61,50 @@ namespace LeetCode.TrappingRainWater
         
         public int Trap(int[] height)
         {
-
+            int leftPointer = 0;
+            int rightPointer = height.Length - 1;
+            int leftValue = height[leftPointer];
+            int rightValue = height[rightPointer];
+            int sum = 0;
+            while (leftPointer != rightPointer)
+            {
+                if (leftValue <= rightValue)
+                {
+                    if (leftValue < height[leftPointer+1])
+                    {
+                        leftPointer = leftPointer + 1;
+                        leftValue = height[leftPointer];
+                    }
+                    else
+                    {
+                        leftPointer++;
+                        int value = leftValue - height[leftPointer];
+                        if (value>0)
+                        {
+                            sum += value;
+                        }
+                    }
+                }
+                else
+                {
+                    if (rightValue < height[rightPointer-1])
+                    {
+                        rightPointer = rightPointer - 1;
+                        rightValue = height[rightPointer];
+                    }
+                    else
+                    {
+                        rightPointer--;
+                        int value = rightValue - height[rightPointer];
+                        if (value>0)
+                        {
+                            sum += value;
+                        }
+                    }
+                }
+            }
             
-
-
-
-
-
-            return 0;
+            return sum;
         }
 
     }
