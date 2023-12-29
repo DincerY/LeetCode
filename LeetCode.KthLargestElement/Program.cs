1fde60785466f524b139948cdb4ac76c3333f3ba
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-29 12:04:34 +0300

    it is not work

---
+++ LeetCode.KthLargestElement/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Net.Security;
+
+Solution solution = new();
+int[] arr = new[] { 7,6,5,4,3,2,1 };
+solution.FindKthLargest(arr, 2);
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    private int HeapSize { get; set; } = 1;
+    private int Result { get; set; }
+    private int[] Arrays { get; set; }
+    
+    private void Swap(int[] arr,int i,int j)    
+    {
+        (arr[i],arr[j]) = (arr[j],arr[i]);
+    }
+
+    private void HeapifyUp(int[] arr,int lastIndex)
+    {
+        int parent = lastIndex / 2;
+        while (lastIndex > 1 && arr[parent] < arr[lastIndex])
+        {
+            Swap(arr,parent,lastIndex);
+            lastIndex = parent;            
+            parent = lastIndex / 2;
+        }
+    }
+
+    private void HeapifyDown(int[] arr,int lastIndex)
+    {
+        int curr = 1;
+        while (curr < lastIndex && arr[curr] < arr[curr * 2] || arr[curr] < arr[curr * 2 + 1])
+        {
+            int left = curr * 2;
+            int right = curr * 2 + 1;
+
+            if (arr[right] > arr[left])
+            {
+                Swap(arr,right,curr);
+                curr = right;
+            }
+            else
+            {
+                Swap(arr,left,curr);
+                curr = left;
+            }
+
+            if (curr >= lastIndex || curr == lastIndex-1)
+            {
+                break;
+            }
+        }
+    }
+
+    private void Insert(int value)
+    {
+        Arrays[HeapSize++] = value;
+        HeapifyUp(Arrays,HeapSize-1);
+    }
+
+    private void Remove()
+    {
+        Result = Arrays[1];
+        Arrays[1] = Arrays[HeapSize];
+        Arrays[HeapSize] = 0;
+        HeapSize--;
+        if (HeapSize <= 1)
+        {
+           return;   
+        }
+        HeapifyDown(Arrays,HeapSize);
+    }
+
+
+    public int FindKthLargest(int[] nums, int k)
+    {
+        if (nums.Length == 1)
+        {
+            return nums[0];
+        }
+        Arrays = new int[nums.Length+1];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Insert(nums[i]);
+        }
+        
+        HeapSize--;
+        for (int i = 0; i < k; i++)
+        {
+            Remove();
+        }
+        return Result;
+    }
+}
--- LeetCode.RegularExpessionMatching/Program.cs
+++ LeetCode.RegularExpessionMatching/Program.cs
@@ -10,7 +10,7 @@ namespace LeetCode.RegularExpessionMatching
         public static void Main(string[] args)
         {
             Solution solution = new();
-            var a = solution.IsMatch("sssafh", "s*h");
+            var a = solution.IsMatchMemoization("aab","c*a*b");
             Console.WriteLine(a);
         }
     }
@@ -18,82 +18,117 @@ namespace LeetCode.RegularExpessionMatching
 
     public class Solution
     {
-        public bool IsMatch(string s, string p)
+        // private Dictionary<(int, int), bool?> cache;
+        // public bool IsMatchWithCache(string s, string p)
+        // {
+        //     cache = new Dictionary<(int, int), bool?>();
+        //     
+        //     
+        //     bool DFS(int i, int j)
+        //     {
+        //         if (cache.ContainsKey((i, j)))
+        //         {
+        //             return cache[(i, j)].Value;
+        //         }
+        //
+        //         bool result;
+        //
+        //         if (i == s.Length && j == p.Length)
+        //         {
+        //             result = true;
+        //         }
+        //         else if (j == p.Length)
+        //         {
+        //             result = false;
+        //         }
+        //         else
+        //         {
+        //             bool match = (i < s.Length) && (s[i] == p[j] || p[j] == '.');
+        //
+        //             if (j + 1 < p.Length && p[j + 1] == '*')
+        //             {
+        //                 result = DFS(i, j + 2) || (match && DFS(i + 1, j));
+        //             }
+        //             else
+        //             {
+        //                 result = match && DFS(i + 1, j + 1);
+        //             }
+        //         }
+        //
+        //         cache[(i, j)] = result;
+        //         return result;
+        //     }
+        //     
+        //
+        //     return DFS(0, 0);
+        // }
+        
+        // public bool IsMatch(string s, string p)
+        // {
+        //     bool DeepForSearch(int i, int j)
+        //     {
+        //         if (i >= s.Length && j>= p.Length)
+        //             return true;
+        //
+        //         if (j>= p.Length)
+        //             return false;
+        //
+        //         bool match = i < s.Length && (s[i] == p[j] || p[j] == '.');
+        //         if ((j+1) < p.Length && p[j+1] == '*')
+        //         {
+        //             return DeepForSearch(i, j + 2) ||              //dont use *
+        //                    (match && DeepForSearch(i + 1, j));     //use *
+        //             
+        //         }
+        //
+        //         if (match)
+        //         {
+        //             return DeepForSearch(i + 1, j + 1);
+        //         }
+        //
+        //         return false;
+        //     }
+        //
+        //     return DeepForSearch(0, 0);
+        //
+        // }
+
+        public bool IsMatchMemoization(string s, string p)
         {
-            bool DeepForSearch(int i, int j)
+            
+            bool Recursive(int i,int j)
             {
-                if (i >= s.Length && j>= p.Length)
+                if (j >= p.Length && i >= s.Length)
+                {
                     return true;
-    
-                if (j>= p.Length)
+                }
+
+                if (j >= p.Length)
+                {
                     return false;
-    
+                }
+
                 bool match = i < s.Length && (s[i] == p[j] || p[j] == '.');
+
                 if ((j+1) < p.Length && p[j+1] == '*')
                 {
-                    return DeepForSearch(i, j + 2) ||              //dont use *
-                           (match && DeepForSearch(i + 1, j));     //use *
-                    
+                    return Recursive(i, j + 2) || (match && Recursive(i + 1, j));
                 }
-    
+
                 if (match)
                 {
-                    return DeepForSearch(i + 1, j + 1);
+                    return Recursive(i +1, j+1);
                 }
-    
                 return false;
             }
-    
-            return DeepForSearch(0, 0);
-    
+            
+
+
+            return Recursive(0,0);
+            
         }
         
-        private Dictionary<(int, int), bool?> cache;
-        public bool IsMatchWithCache(string s, string p)
-        {
-            cache = new Dictionary<(int, int), bool?>();
-            
-            
-            bool DFS(int i, int j)
-            {
-                if (cache.ContainsKey((i, j)))
-                {
-                    return cache[(i, j)].Value;
-                }
 
-                bool result;
 
-                if (i == s.Length && j == p.Length)
-                {
-                    result = true;
-                }
-                else if (j == p.Length)
-                {
-                    result = false;
-                }
-                else
-                {
-                    bool match = (i < s.Length) && (s[i] == p[j] || p[j] == '.');
-
-                    if (j + 1 < p.Length && p[j + 1] == '*')
-                    {
-                        result = DFS(i, j + 2) || (match && DFS(i + 1, j));
-                    }
-                    else
-                    {
-                        result = match && DFS(i + 1, j + 1);
-                    }
-                }
-
-                cache[(i, j)] = result;
-                return result;
-            }
-            
-
-            return DFS(0, 0);
-        }
-        
-       
-        
     }
 }
