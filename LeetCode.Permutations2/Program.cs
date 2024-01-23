c908117b112179162ebe0c78acefc29bfeaf8c85
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-01-23 10:06:18 +0300

    LeetCode'un 47. sorusu olan Permutations II çözüldü fakat hem time complexity hemde space complexity çok fazla biraz düzeltme gerekecek.

--- LeetCode.Permutations2/Program.cs
+++ LeetCode.Permutations2/Program.cs
@@ -1,5 +1,5 @@
 ﻿Solution solution = new Solution();
-var result = solution.PermuteUnique(new []{1,1,2});
+var result = solution.PermuteUnique(new []{1,2,3});
 Console.WriteLine(result);
 
 
@@ -9,8 +9,46 @@ public class Solution {
         List<IList<int>> result = new List<IList<int>>();
         List<int> onePermutation = new List<int>();
 
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+        Recursion(nums, dictionary, result);
+
+        result = RemoveDuplicateLists(result);
+
         return result;
     }
+    
+    private void Recursion(int[] nums,Dictionary<int,int> dictionary,List<IList<int>> result)
+    {  
+        if (dictionary.Count == nums.Length)
+        {
+            result.Add(new List<int>(dictionary.Values));
+            return;
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dictionary.Keys.Contains(i)) continue;
+            dictionary.Add(i,nums[i]);
+            Recursion(nums,dictionary,result);
+            dictionary.Remove(i);
+        }
+    }
+    private List<IList<int>> RemoveDuplicateLists(List<IList<int>> listOfLists)
+    {
+        HashSet<string> set = new HashSet<string>();
+        List<IList<int>> result = new List<IList<int>>();
+
+        foreach (var subList in listOfLists)
+        {
+            // Listeyi string'e çevirerek HashSet içinde kontrol et
+            string listAsString = string.Join(",", subList);
+            if (set.Add(listAsString))
+            {
+                result.Add(subList);
+            }
+        }
+        return result;
+    }
+
     
   
 }
--- LeetCode.Permutations2/obj/Debug/net8.0/LeetCode.Permutations2.AssemblyInfo.cs
+++ LeetCode.Permutations2/obj/Debug/net8.0/LeetCode.Permutations2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.Permutations2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa51e09812500892ea18d8681a0f3dc58fcc4702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98ca76de02346e69060cb5b905cde8fb0f1da503")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.Permutations2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.Permutations2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
