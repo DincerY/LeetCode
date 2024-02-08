bbd64cca001dd50b520358e313b895c803bae9e1
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-08 09:44:15 +0300

    I will add one more solution

--- LeetCode. SpiralMatrix/Program.cs
+++ LeetCode. SpiralMatrix/Program.cs
@@ -1,20 +1,36 @@
 ﻿Solution solution = new();
 
-solution.SpiralOrder(new []
+// solution.SpiralOrder(new []
+// {
+//     new []{1,2,3,4,5,6},
+//     new []{7,8,9,10,11,12},
+//     new []{13,14,15,16,17,18},
+//     new []{19,20,21,22,23,24},
+//     new []{25,26,27,28,29,30},
+//     new []{31,32,33,34,35,36},
+// });
+
+// solution.SpiralOrder(new []
+// {
+//     new []{1,2,3,4},
+//     new []{5,6,7,8},
+//     new []{9,10,11,12}
+//
+// });
+
+var result = solution.SpiralOrder(new[]
 {
-    new []{1,2,3,4,5,6},
-    new []{7,8,9,10,11,12},
-    new []{13,14,15,16,17,18},
-    new []{19,20,21,22,23,24},
-    new []{25,26,27,28,29,30},
-    new []{31,32,33,34,35,36},
+    new[] { 7 },
+    new[] { 9 },
+    new[] { 6 }
 });
 
 
 Console.WriteLine("Hello, World!");
 
 
-public class Solution {
+public partial class Solution
+{
     public IList<int> SpiralOrder(int[][] matrix)
     {
         List<int> result = new();
@@ -24,43 +40,66 @@ public class Solution {
         int top = 0;
         int bottom = 0;
         int d = 0;
-        while (d < 2)
+        if (columnAndRow.column == 1)
+        {
+            for (int i = 0; i < columnAndRow.row; i++)
+            {
+                result.Add(matrix[i][0]);
+            }
+
+            return result;
+        }
+        if (columnAndRow.row == 1)
+        {
+            for (int i = 0; i < columnAndRow.column; i++)
+            {
+                result.Add(matrix[0][i]);
+            }
+
+            return result;
+        }
+        
+        while (d < columnAndRow.row)
         {
             for (int i = left; i < columnAndRow.column - right; i++)
             {
                 result.Add(matrix[top][i]);
             }
+
             top++;
 
 
             for (int i = top; i < columnAndRow.row - bottom; i++)
             {
-                result.Add(matrix[i][columnAndRow.column-1-bottom]);
+                result.Add(matrix[i][columnAndRow.column - 1 - bottom]);
             }
+
             right++;
 
 
-            for (int i = columnAndRow.column - 1-right; i >= left; i--)
+            for (int i = columnAndRow.column - 1 - right; i >= left; i--)
             {
-                result.Add(matrix[columnAndRow.row-1][i]);
+                if (top + bottom != columnAndRow.row)
+                {
+                    result.Add(matrix[columnAndRow.row - 1 - bottom][i]);
+                }
             }
+
             bottom++;
 
             for (int i = columnAndRow.row - 1 - bottom; i >= top; i--)
             {
                 result.Add(matrix[i][left]);
             }
+
             left++;
             d++;
         }
-        
-        
-       
-        
+
         return result;
     }
 
-    private (int row,int column) FindColumnAndRow(int[][] matrix)
+    private (int row, int column) FindColumnAndRow(int[][] matrix)
     {
         int row = matrix.Length;
         int column = matrix[0].Length;
@@ -69,3 +108,11 @@ public class Solution {
     }
 }
 
+public partial class Solution
+{
+    public IList<int> SpiralOrder2(int[][] matrix)
+    {
+        return null;
+    }
+
+}
