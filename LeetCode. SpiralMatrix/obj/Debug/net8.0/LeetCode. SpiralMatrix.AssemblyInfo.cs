aaa0e44eed88facc9fa19071e35e8d5c4d1f8ab1
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-10 09:44:10 +0300

    Ufak bir ekleme

--- LeetCode. SpiralMatrix/obj/Debug/net8.0/LeetCode. SpiralMatrix.AssemblyInfo.cs
+++ LeetCode. SpiralMatrix/obj/Debug/net8.0/LeetCode. SpiralMatrix.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode. SpiralMatrix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd64cca001dd50b520358e313b895c803bae9e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode. SpiralMatrix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode. SpiralMatrix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LeetCode.JumpGame/Program.cs
+++ LeetCode.JumpGame/Program.cs
@@ -2,19 +2,15 @@
 solution.CanJump(new[] { 2, 3, 1, 1, 4 });
 
 
-
 Console.WriteLine("Hello, World!");
-
-
-
-
-
 
 
 
 /// <summary>
 /// Kaç atlama hakkım varsa hepsini küçükten başlayarak denicem ve kaydedicem
 /// Recursive kullarak her atlama hakkımın içine ayrı ayrı girerek işlem yapıcam
+/// Greedy olmasının sebebi üç kısımına gelince birden değilde en büyükten yani üçten devam etmek
+/// üçten devam edilmediğinde de oluyor fakat Greedy algoritması en büyüğü kullanır.
 /// </summary>
 public class Solution {
     public bool CanJump(int[] nums)
@@ -22,10 +18,16 @@ public class Solution {
         int endIndex = nums.Length - 1;
         int canJump = nums[0];
 
-        void Recursive()
+        void Recursive(int index)
         {
-            
+            for (int i = 1; i <= nums[index]; i++)
+            {
+                Recursive(index++);
+            }
         }
+        
+        Recursive(0);
+        
         return true;
     }
 }
