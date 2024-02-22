65b128de63ed6346fcb002103bed85dd13301aff
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-22 09:36:10 +0300

    .D

---
+++ LeetCode.MergeIntervals/Program.cs
@@ -0,0 +1,67 @@
+﻿Solution solution = new();
+solution.Merge(new[]
+{
+    new[] { 1, 3 },
+    new[] { 2, 6 },
+    new[] { 8, 10 },
+    new[] { 10, 12 },
+    new[] { 15, 18 }
+});
+
+
+Console.WriteLine("Hello, World!");
+
+
+public partial class Solution
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        int currArr = 0;
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            if (intervals[currArr][1] >= intervals[i][0])
+            {
+                intervals[currArr][1] = intervals[i][1];
+                intervals[i] = null;
+            }
+            else
+            {
+                currArr = i;
+            }
+        }
+        
+
+        int[][] result = new int[NotNullValueCount(intervals)][];
+
+        return NotNullMatrix(intervals,result);
+    }
+
+    private int NotNullValueCount(int[][] arr)
+    {
+        int loop = 0;
+        foreach (var interval in arr)
+        {
+            if (interval != null)
+            {
+                loop++;
+            }
+        }
+        return loop;
+    }
+
+    private int[][] NotNullMatrix(int[][] arr, int[][] result)
+    {
+        int loop = 0;
+        foreach (var interval in arr)
+        {
+            if (interval != null)
+            {
+                result[loop] = interval;
+                loop++;
+            }
+        }
+
+        return result;
+    }
+    
+}
--- LeetCode.MergeIntervals/obj/Debug/net8.0/LeetCode.MergeIntervals.AssemblyInfo.cs
+++ LeetCode.MergeIntervals/obj/Debug/net8.0/LeetCode.MergeIntervals.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.JumpGame")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.MergeIntervals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.JumpGame")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.JumpGame")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb9568f1740eb4b671a61422f0015a65bdad9b3")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.MergeIntervals")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.MergeIntervals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
