2e53fbe338eeb6bc45371bdce43e51ce2b2f927a
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-28 09:53:47 +0300

    End Of Solution

---
+++ LeetCode.LengthOfLastWord/Program.cs
@@ -0,0 +1,45 @@
+﻿Solution solution = new();
+//solution.LengthOfLastWord("Hello World");
+solution.LengthOfLastWord3("   fly me   to   the moon  ");
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+public partial class Solution {
+    public int LengthOfLastWord(string s)
+    {
+        return s.Trim(' ').Split(' ')[^1].Length;
+    }
+}
+
+public partial class Solution {
+    //This is not my solution
+    public int LengthOfLastWord2(string s)
+    {
+        s = s.Trim();
+        string lastWord = s.Substring(s.LastIndexOf(' ') + 1);
+        return lastWord.Length;
+    }
+}
+
+public partial class Solution {
+    public int LengthOfLastWord3(string s)
+    {
+        int i = s.Length - 1;
+        int length = 0;
+        while (s[i] == ' ')
+        {
+            i -= 1;
+        }
+        while (i >= 0 && s[i] != ' ')
+        {
+            length += 1;
+            i--;
+        }
+        return length;
+    }
+}
