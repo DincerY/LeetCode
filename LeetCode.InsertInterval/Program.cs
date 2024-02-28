f27fd6d1b816ae4cec4156319a18b6d4e2d815aa
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-28 09:18:27 +0300

    .D

--- LeetCode.InsertInterval/Program.cs
+++ LeetCode.InsertInterval/Program.cs
@@ -101,35 +101,6 @@ public partial class Solution
     }
 }
 
-public partial class Solution
-{
-    //It is not my solution
-    public int[][] Insert3(int[][] intervals, int[] newInterval) {
-        var res = new List<int[]>();
-        
-        foreach (var interval in intervals)
-        {
-            if (interval[0] > newInterval[1])
-            {
-                res.Add(newInterval);
-                newInterval = interval;
-            }
-            else if (interval[1] < newInterval[0])
-            {
-                res.Add(interval);
-            }
-            else
-            {
-                newInterval[0] = Math.Min(newInterval[0], interval[0]);
-                newInterval[1] = Math.Max(newInterval[1], interval[1]);
-            }
-        }
-        
-        res.Add(newInterval);
-        
-        return res.ToArray();
-    }
-}
 
 
 
