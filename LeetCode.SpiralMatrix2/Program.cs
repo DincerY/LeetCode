a809257507788418843ff30c67a2b27afe1dec68
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-02-29 09:25:43 +0300

    End of Solution

---
+++ LeetCode.SpiralMatrix2/Program.cs
@@ -0,0 +1,59 @@
+﻿Solution solution = new();
+solution.GenerateMatrix(3);
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+public class Solution {
+    public int[][] GenerateMatrix(int n)
+    {
+        int addValue = 1;
+        int left = 0;
+        int right = n - 1;
+        int top = 0;
+        int bottom = n - 1;
+        
+        int[][] matrix = new int[n][];
+        for (int i = 0; i < n ; i++)
+        {
+            matrix[i] = new int[n];
+        }
+        
+        while (bottom >= top || left <= right)
+        {
+            for (int j = left; j <= right ; j++)
+            {
+                matrix[top][j] = addValue;
+                addValue++;
+            }
+            top++;
+
+            for (int j = top; j <= bottom; j++)
+            {
+                matrix[j][right] = addValue;
+                addValue++;
+            }
+            right--;
+
+            for (int j = right; j >= left; j--)
+            {
+                matrix[bottom][j] = addValue;
+                addValue++;
+            }
+            bottom--;
+
+            for (int j = bottom; j >= top; j--)
+            {
+                matrix[j][left] = addValue;
+                addValue++;
+            }
+
+            left++;
+        }
+
+        return matrix;
+    }
+}   
--- LeetCode.SpiralMatrix2/obj/Debug/net8.0/LeetCode.SpiralMatrix2.AssemblyInfo.cs
+++ LeetCode.SpiralMatrix2/obj/Debug/net8.0/LeetCode.SpiralMatrix2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.InsertInterval")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.SpiralMatrix2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.InsertInterval")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.InsertInterval")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e53fbe338eeb6bc45371bdce43e51ce2b2f927a")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.SpiralMatrix2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.SpiralMatrix2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
