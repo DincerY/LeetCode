2d395ed263d2de2b5f8de342088a2606a3febae7
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-03-15 10:33:08 +0300

    .D

--- LeetCode.AddBinary/Program.cs
+++ LeetCode.AddBinary/Program.cs
@@ -1,5 +1,5 @@
 ﻿Solution solution = new();
-solution.AddBinary("1111", "1111");
+solution.AddBinary("10001", "11");
 
 
 Console.WriteLine("Hello, World!");
@@ -7,55 +7,29 @@ Console.WriteLine("Hello, World!");
 
 public class Solution
 {
-    /// <summary>
-    /// Devamında charların sayı değerlerini hesaplıcam.
-    /// </summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    public string AddBinary(string a, string b)
+   
+    public string AddBinary(string a, string b) {
+        string res = "";
+        int carry = 0;
+        for (int i = 0; i < Math.Max(a.Length, b.Length); i++) {
+            int digitA = i < a.Length ? (int)(a[i] - '0') : 0;
+            int digitB = i < b.Length ? (int)(b[i] - '0') : 0;
+            int total = digitA + digitB + carry;
+            res = (total % 2).ToString() + res;
+            carry = total / 2;
+        }
+        if (carry > 0)
+            res = "1" + res;
+        return res;
+    }
+
+    public string SumChar(string a, string b)
     {
-        int length = Math.Max(a.Length, b.Length);
-        int inta = int.Parse(a);
-        int intb = int.Parse(b);
-
-        var intResult = inta + intb;
-        
-
-        var result = (inta + intb).ToString().ToCharArray();
-
-        for (int i = length - 1; i >= 0; i--)
+        for (int i = 0; i < Math.Max(a.Length,b.Length); i++)
         {
-            if (result[i] >= 50)
-            {
-                if (i-1 < 0)
-                {
-                    result[0] = '0';
-                    List<char> newChar = new List<char>(length +1);
-                    newChar.Add('1');
-                    newChar.AddRange(result);
-                    var charArr = newChar.ToArray();
-                    return new string(charArr);
-                }
-                else
-                {
-                    result[i] = '0';
-                    if (result[i - 1] == '1')
-                    {
-                        result[i - 1] = '2';
-                    }
-                    else if (result[i -1] == '2')
-                    {
-                        result[i -1] = '3';
-                    }
-                    else
-                    {
-                        result[i - 1] = '1';
-                    }
-                }
-            }
+            
         }
 
-        return new string(result);
+        return "";
     }
 }
--- LeetCode.AddBinary/obj/Debug/net8.0/LeetCode.AddBinary.AssemblyInfo.cs
+++ LeetCode.AddBinary/obj/Debug/net8.0/LeetCode.AddBinary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.AddBinary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f66df4568b6c49391f3c3c2e907db6177e291e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a49d0b5070a0ba0a4377df2e82a304163dcb1bc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.AddBinary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.AddBinary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
