b46f0e025c6e7a144eb8cc61620d662fa23d3f59
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-03-20 10:27:52 +0300

    To be continue

---
+++ LeetCode.TextJustification/Program.cs
@@ -0,0 +1,71 @@
+﻿Solution solution = new();
+// solution.FullJustify(new[]
+// {
+//     "This", "is", "an", "example", "of", "text", "justification."
+// }, 16);
+
+// solution.FullJustify(new[]
+// {
+//     "What","must","be","acknowledgment","shall","be"
+// }, 16);
+
+solution.FullJustify(new[]
+{
+    "Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"
+}, 20);
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public IList<string> FullJustify(string[] words, int maxWidth)
+    {
+        //her string değerinin sonunda bir boşluk var
+        List<string> result = new();
+        StringInList(words, maxWidth, result);
+
+        foreach (var rslt in result)
+        {
+            int length = rslt.Length - 1;
+            var deneme = rslt.Split(' ');
+            
+        }
+
+        return null;
+        
+    }
+    
+    private void StringInList(string[] words, int maxWidth, List<string> result)
+    {
+        string tempData = "";
+        int sum = 0;
+
+        foreach (var word in words)
+        {
+            if (sum + word.Length <= maxWidth)
+            {
+                sum += word.Length + 1;
+                tempData += word;
+                tempData += " ";
+            }
+            else
+            {
+                result.Add(tempData);
+                tempData = "";
+                sum = 0;
+                sum += word.Length + 1;
+                tempData += word;
+                tempData += " ";
+            }
+        }
+
+        if (tempData != "")
+        {
+            result.Add(tempData); 
+        }
+    }
+
+    
+}
--- LeetCode.TextJustification/obj/Debug/net8.0/LeetCode.TextJustification.AssemblyInfo.cs
+++ LeetCode.TextJustification/obj/Debug/net8.0/LeetCode.TextJustification.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.AddBinary")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.TextJustification")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+817316dac94541357513ea76b9ee870817463f9f")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.AddBinary")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.AddBinary")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.TextJustification")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.TextJustification")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
