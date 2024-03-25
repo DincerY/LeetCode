6774ec8b5fcbfddd68695b6ee8126c583e746106
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-03-25 09:49:35 +0300

    Add Solution Different Ways

--- LeetCode.Sqrt/Program.cs
+++ LeetCode.Sqrt/Program.cs
@@ -1,15 +1,12 @@
 ﻿Solution solution = new();
-solution.MySqrt(2147395600);
+//solution.MySqrt2(2147395599);
 //2 147 483 647
 
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine(solution.MySqrt3(2147395599));
 
 
-
-
-
-public class Solution {
+public partial class Solution {
     public int MySqrt(int x)
     {
         if (x >= 2147395600)
@@ -36,5 +33,56 @@ public class Solution {
             return 46340;
         }
         return i -1;
+    }
+}
+
+public partial class Solution {
+    public int MySqrt2(int x)
+    {
+        if (x == 0)
+        {
+            return 0;
+        }
+        int left = 0;
+        int right = x - 1;
+        int mid = right / 2;
+        while (right - left != 1 && left < right)
+        {
+            if ((mid + 1) * (mid + 1) > x)
+            {
+                right = mid ;
+            }
+            else
+            {
+                left = mid;
+            }
+
+            mid = (left + right) / 2;
+        }
+
+        return left +1;
+    }
+}
+
+/// <summary>
+/// NeedCode solution
+/// </summary>
+public partial class Solution {
+    public int MySqrt3(int x) {
+        int res = 0;
+        int l = 0;
+        int r = x;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if ((long)m * m == x) {
+                return m;
+            } else if ((long)m * m < x) {
+                res = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return res;
     }
 }
--- LeetCode.TextJustification/obj/Debug/net8.0/LeetCode.TextJustification.AssemblyInfo.cs
+++ LeetCode.TextJustification/obj/Debug/net8.0/LeetCode.TextJustification.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.TextJustification")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c53253da060789991cebc53133562676135b0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.TextJustification")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.TextJustification")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
