03d593ed930794cb41dc2ea0cd1177bc5ffefc05
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-04-29 09:34:32 +0300

    added new solution

---
+++ LeetCode.Combinations/Program.cs
@@ -0,0 +1,23 @@
+﻿Solution solution = new();
+
+solution.Combine(4, 2);
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+public class Solution {
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        //Ele alınan sayı kendisi ve gerisinde ki sayı dahil edilmeden işlenir.
+        List<IList<int>> res = new List<IList<int>>();
+        
+        
+        
+
+        return res;
+    }
+}
