3b4b0ebde3cf06bd9654fc5a9d98bada8416786a
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-05-08 09:41:16 +0300

    new solution but not yet accepted

---
+++ LeetCode.SearchRotatedSortedArrayII/Program.cs
@@ -0,0 +1,65 @@
+﻿Solution solution = new();
+// solution.Search(new[]
+// {
+//     2, 5, 6, 0, 0, 1, 2
+// }, 3);
+
+solution.Search(new[]
+{
+    2,2,2,3,2,2,2
+}, 3);
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public bool Search(int[] nums, int target)
+    {
+        //indexin kendisi ilk arrayın sonu iken indexin bir fazlası yeni arrayın başı
+        int index = 0;
+        while (index + 1 < nums.Length && nums[index] <= nums[index + 1])
+        {
+            index++;
+        }
+
+        if (nums.Length == 1)
+        {
+            return nums[0] == target ? true : false;
+        }
+
+        if (nums[0] < target)
+        {
+            return BinarySearch(nums, target, 0, index);
+        }
+        else
+        {
+            return BinarySearch(nums, target, index + 1, nums.Length - 1);
+        }
+    }
+
+    private bool BinarySearch(int[] arr, int target, int low, int high)
+    {
+        int left = low;
+        int right = high;
+        while (left <= right)
+        {
+            int middle = (left + right) / 2;
+            if (arr[middle]< target)
+            {
+                left = middle + 1;
+            }
+            else if (arr[middle] > target)
+            {
+                right = middle - 1;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- LeetCode.SearchRotatedSortedArrayII/obj/Debug/net8.0/LeetCode.SearchRotatedSortedArrayII.AssemblyInfo.cs
+++ LeetCode.SearchRotatedSortedArrayII/obj/Debug/net8.0/LeetCode.SearchRotatedSortedArrayII.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.RemoveDuplicatesII")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.SearchRotatedSortedArrayII")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afaf49a2ad67a827d6e100522fd4117df0c4c885")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.RemoveDuplicatesII")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.RemoveDuplicatesII")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.SearchRotatedSortedArrayII")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.SearchRotatedSortedArrayII")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
