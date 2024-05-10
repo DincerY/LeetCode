0ed57bb330de55b499a6da77bfa51e2146fef1e1
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-05-10 09:23:34 +0300

    new solution

---
+++ LeetCode.RemoveDuplicatesSortedList/Program.cs
@@ -0,0 +1,42 @@
+﻿Solution solution = new Solution();
+solution.DeleteDuplicates(new ListNode(1, 
+    new ListNode(1, 
+        new ListNode(2,
+            new ListNode(3,
+                new ListNode(3))))));
+
+Console.WriteLine("Hello, World!");
+
+
+public class ListNode{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        ListNode left = head;
+        ListNode right = head.next;
+        while (right != null)   
+        {
+            if (left.val != right.val)
+            {
+                left.next = right;
+                left = right;
+                right = right.next;
+
+            }
+            else
+            {
+                right = right.next;
+            }
+        }
+
+        return head;
+    }
+}
