e0436764022104a0e5f1d801c5baa531de29adad
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-06-06 10:19:13 +0300

    To be continue

---
+++ LeetCode.BinaryTreeInorderTraversal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+Solution solution = new();
+solution.InorderTraversal(new TreeNode(1,null,new TreeNode(2,new TreeNode(3),null)));
+
+Console.WriteLine("Hello, World!");
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        List<int> result = new();
+
+        void Dfs(TreeNode tree)
+        {
+            if (tree != null)
+            {
+                result.Add(tree.val);
+                Dfs(tree.left);
+                Dfs(tree.right);
+            }
+        }
+        
+        Dfs(root);
+        return result;
+    }
+}
--- LeetCode.BinaryTreeInorderTraversal/obj/Debug/net8.0/LeetCode.BinaryTreeInorderTraversal.AssemblyInfo.cs
+++ LeetCode.BinaryTreeInorderTraversal/obj/Debug/net8.0/LeetCode.BinaryTreeInorderTraversal.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.RestoreIPAddresses")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.BinaryTreeInorderTraversal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0b673d906b1be56fb809ad967c0a82f7041f07")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.RestoreIPAddresses")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.RestoreIPAddresses")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.BinaryTreeInorderTraversal")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.BinaryTreeInorderTraversal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
