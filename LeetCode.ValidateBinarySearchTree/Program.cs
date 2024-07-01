152df73ebd7f06f435f991af0bf44de5191bfe07
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-07-01 10:20:16 +0300

    added new problem to be continue

---
+++ LeetCode.ValidateBinarySearchTree/Program.cs
@@ -0,0 +1,25 @@
+﻿Solution solution = new();
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public bool IsValidBST(TreeNode root)
+    {
+        return true;
+    }
+}
