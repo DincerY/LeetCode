7277db09fc70f155a1279693e4c62e2f790fd72b
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-07-05 10:12:20 +0300

    Accepted

---
+++ LeetCode.SameTree/Program.cs
@@ -0,0 +1,51 @@
+﻿Solution solution = new Solution();
+// solution.IsSameTree(new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+//     new TreeNode(1, new TreeNode(2), new TreeNode(3)));
+
+
+solution.IsSameTree(new TreeNode(1, new TreeNode(1)),
+    new TreeNode(1, null, new TreeNode(1)));
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        List<int> list = new();
+        void Dfs(TreeNode node)
+        {
+            if (node == null)
+            {
+                list.Add(int.MinValue);
+                return;
+            }
+            list.Add(node.val);
+
+            Dfs(node.left);
+            Dfs(node.right);
+        }
+            
+        Dfs(p);
+        var newList = list.ToList();
+        list = new();
+        Dfs(q);
+        return newList.SequenceEqual(list);
+    }
+}
--- LeetCode.SameTree/obj/Debug/net8.0/LeetCode.SameTree.AssemblyInfo.cs
+++ LeetCode.SameTree/obj/Debug/net8.0/LeetCode.SameTree.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.RecoverBinarySearchTree")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.SameTree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f46c0e052debfe4a13671f65893895b0b23c5c")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.RecoverBinarySearchTree")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.RecoverBinarySearchTree")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.SameTree")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.SameTree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
