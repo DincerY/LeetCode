201309fd1fa8d0b89423642bb3e9e31ea79f721f
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-07-08 15:21:17 +0300

    add bfs to be continue

--- LeetCode.SymmetricTree/Program.cs
+++ LeetCode.SymmetricTree/Program.cs
@@ -2,9 +2,18 @@
 solution.IsSymmetric(new TreeNode(1,
     new TreeNode(2,
         new TreeNode(3), new TreeNode(4))
-    ,new TreeNode(2, 
+    , new TreeNode(2,
         new TreeNode(4), new TreeNode(3))));
 
+TreeNode root = new TreeNode(10);
+root.left = new TreeNode(5);
+root.right = new TreeNode(15);
+root.left.left = new TreeNode(3);
+root.left.right = new TreeNode(7);
+root.right.right = new TreeNode(18);
+
+// Ağacı BFS ile dolaşın
+solution.BFS(root);
 
 Console.WriteLine("Hello, World!");
 
@@ -23,17 +32,41 @@ public class TreeNode
     }
 }
 
-public class Solution
+public partial class Solution
 {
     public bool IsSymmetric(TreeNode root)
     {
-        
         return true;
     }
     
-    public void BFS(TreeNode root)
-    {
-        
-    }
 }
 
+public partial class Solution
+{
+    public void BFS(TreeNode root)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            TreeNode currentNode = queue.Dequeue();
+            Console.WriteLine(currentNode.val); // Düğüm değerini yazdırın
+
+            if (currentNode.left != null)
+            {
+                queue.Enqueue(currentNode.left);
+            }
+
+            if (currentNode.right != null)
+            {
+                queue.Enqueue(currentNode.right);
+            }
+        }
+    }
+}
--- LeetCode.SymmetricTree/obj/Debug/net8.0/LeetCode.SymmetricTree.AssemblyInfo.cs
+++ LeetCode.SymmetricTree/obj/Debug/net8.0/LeetCode.SymmetricTree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.SymmetricTree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7277db09fc70f155a1279693e4c62e2f790fd72b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f86ba181422e114e3d8260b8ff28059cedb7461")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.SymmetricTree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.SymmetricTree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
