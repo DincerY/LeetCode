73aacd948e02011f2d42e09f4b28772e25f0471d
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-08-16 09:56:52 +0300

    accepted

--- LeetCode.BestTimeToBuyAndSellStock/Program.cs
+++ LeetCode.BestTimeToBuyAndSellStock/Program.cs
@@ -1,5 +1,5 @@
 ﻿Solution solution = new();
-solution.MaxProfit2(
+solution.MaxProfit3(
     new[] { 7, 1, 5, 3, 6, 4 });
 // solution.MaxProfit2(
 //     new[] { 7, 6, 4, 3 , 1 });
@@ -9,7 +9,8 @@ Console.WriteLine("Hello, World!");
 
 
 //time limit exceeded
-public partial class Solution {
+public partial class Solution
+{
     public int MaxProfit(int[] prices)
     {
         int profit = 0;
@@ -24,23 +25,47 @@ public partial class Solution {
             }
         }
 
-
         return profit;
     }
 }
 
-public partial class Solution {
+public partial class Solution
+{
     public int MaxProfit2(int[] prices)
     {
         int profit = 0;
         for (int i = 1; i < prices.Length; i++)
         {
-            if (prices[i] > prices[i -1])
+            if (prices[i] > prices[i - 1])
             {
                 profit = Math.Max(profit, prices[i] - prices[i - 1]);
                 prices[i] = prices[i - 1];
             }
         }
+
         return profit;
+    }
+}
+
+//it is not mine solution
+public partial class Solution
+{
+    public int MaxProfit3(int[] prices)
+    {
+        int l = 0, r = 1;
+        int maxP = 0;
+        while (r < prices.Length)
+        {
+            if (prices[l] < prices[r])
+            {
+                maxP = Math.Max(maxP, prices[r] - prices[l]);
+            }
+            else
+            {
+                l = r;
+            }
+            r++;
+        }
+        return maxP;
     }
 }
--- LeetCode.Triangle/obj/Debug/net8.0/LeetCode.Triangle.AssemblyInfo.cs
+++ LeetCode.Triangle/obj/Debug/net8.0/LeetCode.Triangle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.Triangle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0447e2a315ac0b8a019fc9d01dbb19e963222b04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.Triangle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.Triangle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
