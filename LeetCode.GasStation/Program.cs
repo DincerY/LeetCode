7bcb61aa78123e373e98769ca0fd2b6842d88687
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-08-26 13:57:30 +0300

    To be continue

---
+++ LeetCode.GasStation/Program.cs
@@ -0,0 +1,50 @@
+﻿Solution solution = new();
+solution.CanCompleteCircuit(new []
+{
+    1,2,3,4,5
+},new []
+{
+    3,4,5,1,2
+});
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        int startIdx = 0;
+        int tank = 0;
+        for (int i = 0; i < gas.Length; i++)
+        {
+            if (gas[i] > cost[i])
+            {
+                startIdx = i;
+                break;
+            }
+        }
+
+        int j = 0;
+        tank += gas[startIdx];
+        startIdx++;
+        while (j != startIdx)
+        {
+            if (startIdx >= gas.Length )
+            {
+                j = 0;
+            }
+
+            tank -= cost[startIdx];
+            if (tank < 0)
+            {
+                break;
+            }
+            tank += gas[startIdx];
+
+            j++;
+        }
+        return startIdx;
+    }
+}
