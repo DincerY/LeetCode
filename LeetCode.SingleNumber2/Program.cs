9f18d24df55393b8a6624b9aa3cf250a9478cba7
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-08-29 13:36:03 +0300

    accepted

---
+++ LeetCode.SingleNumber2/Program.cs
@@ -0,0 +1,29 @@
+﻿Solution solution = new();
+//solution.SingleNumber(new[] { 0, 1, 0, 1, 0, 1, 99 });
+solution.SingleNumber(new[] { 2,2,3,2 });
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        Array.Sort(nums);
+        int i;
+        for (i = 0; i < nums.Length-1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                i += 2;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return nums[i];
+    }
+}
