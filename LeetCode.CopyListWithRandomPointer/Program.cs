29843be02d0a7460a00432ba62ff3a25b2ce5101
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-08-29 13:53:34 +0300

    To be continue

---
+++ LeetCode.CopyListWithRandomPointer/Program.cs
@@ -0,0 +1,40 @@
+﻿Solution solution = new();
+solution.CopyRandomList(new Node(7).next = new Node(13));
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Node
+{
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int _val)
+    {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
+
+
+public class Solution
+{
+    public Node CopyRandomList(Node head)
+    {
+        Node temp = head.next;
+        Node newHead = new Node(head.val);
+        newHead.random = head.random;
+        Node newTemp = newHead;
+        while (temp == null)
+        {
+            newTemp.next = new Node(temp.val);
+            newTemp.random = new Node(temp.random.val);
+            temp = temp.next;
+            newTemp = newTemp.next;
+        }
+        return newHead;
+    }
+}
