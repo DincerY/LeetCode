f218852c04bee4b587bb19b89007c0b0625b5052
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-04 13:34:15 +0300

    removed unnecessary if else

--- LeetCode.LRUCache/Program.cs
+++ LeetCode.LRUCache/Program.cs
@@ -1,8 +1,9 @@
-﻿LRUCache lruCache = new(10);
+﻿LRUCache lruCache = new(2);
+//LRUCache2 lruCache = new(10);
 // lruCache.Put(1, 1);
 // lruCache.Put(2, 2);
 // lruCache.Get(1);
-// lruCache.Put(3, 3);
+// lruCache.Put(3, 3);  
 // lruCache.Get(2);
 // lruCache.Put(4, 4);
 // lruCache.Get(1);
@@ -53,40 +54,23 @@ public class LRUCache
 
     public void Put(int key, int value)
     {
-        if (_dic.Count >= _capacity)
+        if (_dic.ContainsKey(key))
         {
-            if (_dic.ContainsKey(key))
-            {
-                _dic.Remove(key);
-                _dic.Add(key, value);
-                int delKey = Array.IndexOf(_list.ToArray(), key);
-                _list.RemoveAt(delKey);
-                _list.Add(key);
-            }
-            else
+            _dic.Remove(key);
+            _dic.Add(key,value);
+            int delKey = Array.IndexOf(_list.ToArray(), key);
+            _list.RemoveAt(delKey);
+            _list.Add(key);
+        }
+        else
+        {
+            if (_dic.Count >= _capacity)
             {
                 int deletedKey = _list[0];
                 _list.RemoveAt(0);
                 _dic.Remove(deletedKey);
-
-                _dic.Add(key, value);
-                _list.Add(key);
             }
-        }
-        else
-        {
-            if (_dic.ContainsKey(key))
-            {
-                _dic.Remove(key);
-            }
-
             _dic.Add(key, value);
-            if (_list.Contains(key))
-            {
-                int delKey = Array.IndexOf(_list.ToArray(), key);
-                _list.RemoveAt(delKey);
-            }
-
             _list.Add(key);
         }
     }
