f1a3a780a9b1a235eb4d7ae0b6fd8601758cd14c
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-04 09:30:25 +0300

    accepted

---
+++ LeetCode.BinaryTreePostorderTraversal/Program.cs
@@ -0,0 +1,40 @@
+﻿Solution solution = new();
+solution.PostorderTraversal(new TreeNode(1,null,new TreeNode(2,new TreeNode(3))));
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
+        List<int> list = new();
+        void Dfs(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Dfs(node.left);
+            Dfs(node.right);
+            list.Add(node.val);
+        }
+        Dfs(root);
+        return list;
+    }
+}
