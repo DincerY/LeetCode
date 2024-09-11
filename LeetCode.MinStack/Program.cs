a63bc87f5b90b835cf77b7b18d93142e1c4010dd
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-11 12:48:26 +0300

    accepted

---
+++ LeetCode.MinStack/Program.cs
@@ -0,0 +1,50 @@
+﻿MinStack minStack = new();
+minStack.Push(-2);
+minStack.Push(0);
+minStack.Push(-3);
+minStack.GetMin(); 
+minStack.Pop();
+minStack.Top();    
+minStack.GetMin(); 
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class MinStack
+{
+    private List<int> _list;
+
+    private int _tailIndex;
+    public MinStack()
+    {
+        _list = new List<int>();
+        _tailIndex = 0;
+    }
+
+    public void Push(int val)
+    {
+        _list.Add(val);
+        _tailIndex++;
+    }
+
+    public void Pop()
+    {
+        if (_tailIndex == 0)
+        {
+            return;
+        }
+        _list.RemoveAt(_tailIndex-1);
+        _tailIndex--;
+    }
+
+    public int Top()
+    {
+        return _list[_tailIndex-1];
+    }
+
+    public int GetMin()
+    {
+        return _list.Min();
+    }
+}
