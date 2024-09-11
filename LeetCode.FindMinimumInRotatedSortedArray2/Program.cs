5b1312f9fe100a71de629f4405c37769392a84fe
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-11 12:24:59 +0300

    accepted

---
+++ LeetCode.FindMinimumInRotatedSortedArray2/Program.cs
@@ -0,0 +1,44 @@
+﻿Solution solution = new();
+solution.FindMin2(new[]
+{
+    4, 4, 0, 1, 2, 3
+});
+
+
+Console.WriteLine("Hello, World!");
+
+public partial class Solution
+{
+    public int FindMin(int[] nums)
+    {
+        Array.Sort(nums);
+        return nums[0];
+    }
+}
+
+//it is not mine solution
+public partial class Solution
+{
+    public int FindMin2(int[] nums)
+    {
+        int l = 0, r = nums.Length - 1;
+        while (l < r)
+        {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[r])
+            {
+                l = mid + 1;
+            }
+            else if (nums[mid] < nums[l])
+            {
+                r = mid;
+            }
+            else
+            {
+                r--;
+            }
+        }
+
+        return nums[l];
+    }
+}
--- LeetCode.FindMinimumInRotatedSortedArray2/obj/Debug/net8.0/LeetCode.FindMinimumInRotatedSortedArray2.AssemblyInfo.cs
+++ LeetCode.FindMinimumInRotatedSortedArray2/obj/Debug/net8.0/LeetCode.FindMinimumInRotatedSortedArray2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.FindMinimumInRotatedSortedArray")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.FindMinimumInRotatedSortedArray2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189eae97b8a90863522ecdc14f9baeff31168e69")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.FindMinimumInRotatedSortedArray")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.FindMinimumInRotatedSortedArray")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.FindMinimumInRotatedSortedArray2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.FindMinimumInRotatedSortedArray2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
