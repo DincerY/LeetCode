309e74dca2c6e618bc2936fc560cd66cb6df3ae1
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-16 12:22:48 +0300

    accepted

---
+++ LeetCode.ExcelSheetColumnNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿Solution solution = new();
+solution.TitleToNumber("A");
+solution.TitleToNumber("AB");
+solution.TitleToNumber("ZY");
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int TitleToNumber(string columnTitle)
+    {
+        int total = 0;
+        foreach (var chr in columnTitle)
+        {
+            if (total != 0) 
+                total *= 26;
+            total += chr - 64;
+        }
+        return total;
+    }
+}
