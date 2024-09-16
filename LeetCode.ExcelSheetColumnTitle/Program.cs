540d229a9979bb4c45826aca49e56e78397f195f
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-16 11:24:34 +0300

    accepted

---
+++ LeetCode.ExcelSheetColumnTitle/Program.cs
@@ -0,0 +1,22 @@
+﻿Solution solution = new();
+//solution.ConvertToTitle(1);
+//solution.ConvertToTitle(28);
+solution.ConvertToTitle(52);
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution {
+    public string ConvertToTitle(int columnNumber)
+    {
+        string result = "";
+        while (columnNumber > 0) {
+            columnNumber--;
+            char c = (char) ('A' + columnNumber % 26);
+            result = c + result;
+            columnNumber /= 26;
+        }
+        return result;
+    }
+}
