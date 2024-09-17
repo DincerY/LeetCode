f370610a521248a9bfd47292cd160a93fd139526
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-17 09:56:51 +0300

    .(

---
+++ LeetCode.LargestNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+Solution solution = new();
+solution.LargestNumber(new[]
+{
+    3, 30, 34, 5, 9
+});
+
+solution.LargestNumber(new[]
+{
+    1000000000, 1000000000
+});
+
+Console.WriteLine("Hello, World!");
+
+//it is not mine
+//it is not worked
+public class Solution
+{
+    public string LargestNumber(int[] arr)
+    {
+        if (arr.All(_ => _ == 0))
+        {
+            return "0";
+        }
+
+        string result;
+        Array.Sort(arr, delegate(int num1, int num2)
+        {
+            return Convert.ToInt64(Convert.ToString(num2) + Convert.ToString(num1)).CompareTo(Convert.ToInt64(Convert.ToString(num1) + Convert.ToString(num2)));
+        });
+        result = string.Join("", arr);
+        return result;
+    }
+}
