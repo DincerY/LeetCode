ce5385c753f50dd4670d901e754e5435f6efdf32
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-17 10:29:55 +0300

    accepted

---
+++ LeetCode.RepeatedDNASequences/Program.cs
@@ -0,0 +1,26 @@
+﻿Solution solution = new();
+solution.FindRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT");
+
+Console.WriteLine("Hello, World!");
+
+//it is not mine solution
+public class Solution
+{
+    public IList<string> FindRepeatedDnaSequences(string s)
+    {
+        HashSet<string> seen = new HashSet<string>();
+        HashSet<string> res = new HashSet<string>();
+        for (int l = 0; l < s.Length - 9; l++)
+        {
+            string cur = s.Substring(l, 10);
+            if (seen.Contains(cur))
+            {
+                res.Add(cur);
+            }
+
+            seen.Add(cur);
+        }
+
+        return new List<string>(res);
+    }
+}
