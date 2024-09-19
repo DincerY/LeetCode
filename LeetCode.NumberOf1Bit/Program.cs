f5d07a282ef590823b9c97e9e54dc57db3a933fc
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-19 08:58:12 +0300

    accepted

---
+++ LeetCode.NumberOf1Bit/Program.cs
@@ -0,0 +1,22 @@
+﻿Solution solution = new();
+solution.HammingWeight(11);
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int HammingWeight(int n)
+    {
+        int count = 0;
+        int val = (int)Math.Log2(n) + 1;  
+        for (int i = val; i >= 0; i--)
+        {
+            if ((n & (1 << i)) != 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
