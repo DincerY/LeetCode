6e044fb6edfcdff52ae30ba797388101dcdba581
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-24 09:32:08 +0300

    accepted

--- LeetCode.EditDistance/Program.cs
+++ LeetCode.EditDistance/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Solution solution = new();
+solution.MinDistance("horse", "ros");
+solution.MinDistance("intention", "execution");
+
 
 Console.WriteLine("Hello, World!");
+
+
+//NeedCode's solution
+public class Solution
+{
+    public int MinDistance(string word1, string word2)
+    {
+        int[,] cache = new int[word1.Length + 1, word2.Length + 1];
+
+        for (int i = 0; i <= word2.Length; i++)
+        {
+            cache[word1.Length, i] = word2.Length - i;
+        }
+        for (int j = 0; j <= word1.Length; j++)
+        {
+            cache[j, word2.Length] = word1.Length - j;
+        }
+
+        for (int i = word1.Length - 1; i >= 0; i--)
+        {
+            for (int j = word2.Length - 1; j >= 0; j--)
+            {
+                if (word1[i] == word2[j])
+                {
+                    cache[i, j] = cache[i + 1, j + 1];
+                }
+                else
+                {
+                    cache[i, j] = 1 + Math.Min(cache[i + 1, j], Math.Min(cache[i, j + 1], cache[i + 1, j + 1]));
+                }
+            }
+        }
+        return cache[0, 0];
+    }
+}
--- LeetCode.EditDistance/obj/Debug/net8.0/LeetCode.EditDistance.AssemblyInfo.cs
+++ LeetCode.EditDistance/obj/Debug/net8.0/LeetCode.EditDistance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.EditDistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e8143cdf12caa166e7e35a96afba41af88ec80c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.EditDistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.EditDistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
