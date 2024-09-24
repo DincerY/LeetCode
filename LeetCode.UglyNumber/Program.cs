c641bf71de25ede12d3ec79416e0ee5324da7120
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-24 13:28:48 +0300

    accepted

---
+++ LeetCode.UglyNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿Solution solution = new();
+solution.IsUgly(14);
+solution.IsUgly(1);
+solution.IsUgly(6);
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public bool IsUgly(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        List<int> list = new(){2,3,5};
+        foreach (var val in list)
+        {
+            while (n % val == 0)
+            {
+                n /= val;
+            }
+        }
+        return n == 1;
+    }
+}
