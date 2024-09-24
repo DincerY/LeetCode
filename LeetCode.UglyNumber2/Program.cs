5805a76875171947f4b15fc80d65f6c224a7d9c7
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-24 13:40:12 +0300

    To be continue

---
+++ LeetCode.UglyNumber2/Program.cs
@@ -0,0 +1,24 @@
+﻿Solution solution = new();
+solution.NthUglyNumber(10);
+solution.NthUglyNumber(1);
+
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int NthUglyNumber(int n)
+    {
+        int val = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (val % 7 == 0)
+            {
+                
+            }   
+        }
+
+
+        return 0;
+    }
+}
