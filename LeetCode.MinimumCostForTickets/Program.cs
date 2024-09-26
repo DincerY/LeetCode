0adc179a86f187a257e9846ec1c9ce1f7ef54535
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-09-26 14:03:01 +0300

    To be continue

---
+++ LeetCode.MinimumCostForTickets/Program.cs
@@ -0,0 +1,23 @@
+﻿Solution solution = new();
+solution.MincostTickets(new[]
+    {
+        1, 4, 6, 7, 8, 20
+    },
+    new[]
+    {
+        2, 7, 15
+    });
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int MincostTickets(int[] days, int[] costs)
+    {
+
+
+        return 0;
+    }
+}
