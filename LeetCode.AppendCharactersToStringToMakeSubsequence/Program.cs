cafbb2e3d2797603f8d9cb30c0619433302d8378
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-10-03 10:27:47 +0300

    accepted

---
+++ LeetCode.AppendCharactersToStringToMakeSubsequence/Program.cs
@@ -0,0 +1,24 @@
+﻿Solution solution = new();
+solution.AppendCharacters("coaching", "coding");
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int AppendCharacters(string s, string t)
+    {
+        int j = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (j >= t.Length)
+            {
+                break;
+            }
+            if (s[i] == t[j])
+            {
+                j++;
+            }
+        }
+        return t.Length - j;
+    }
+}
