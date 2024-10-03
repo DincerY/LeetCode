1b73cc307ce016917fe23afdaeddd7dfb52fa5c7
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-10-03 10:59:55 +0300

    too hard

---
+++ LeetCode.MaximumProductOfTheLengthOfTwoPalindromicSubsequences/Program.cs
@@ -0,0 +1,14 @@
+﻿Solution solution = new();
+solution.MaxProduct("leetcodecom");
+solution.MaxProduct("accbcaxcx");
+solution.MaxProduct("bb");
+
+Console.WriteLine("Hello, World!");
+
+//I can not solve it it is too hard
+public class Solution {
+    public int MaxProduct(string s) {
+        
+        return 0;
+    }
+}
