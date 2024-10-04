76287f1f8858d13fa3a3bfff2bff8fb73a032c0b
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-10-04 12:58:55 +0300

    accepted

---
+++ LeetCode.TargetSum/Program.cs
@@ -0,0 +1,60 @@
+﻿Solution solution = new();
+solution.FindTargetSumWays2(new[] { 1, 1, 1, 1, 1 }, 3);
+
+
+Console.WriteLine("Hello, World!");
+
+
+public partial class Solution
+{
+    public int FindTargetSumWays(int[] nums, int target)
+    {
+        int res = 0;
+
+        void Dfs(int index, int total)
+        {
+            if (index >= nums.Length)
+            {
+                if (total == target)
+                {
+                    res++;
+                }
+
+                return;
+            }
+
+            Dfs(index + 1, total + nums[index]);
+            Dfs(index + 1, total + -nums[index]);
+        }
+
+        Dfs(0, 0);
+        return res;
+    }
+}
+
+//Needcode solution(Dynamic Programming)
+public partial class Solution
+{
+    public int FindTargetSumWays2(int[] nums, int target)
+    {
+        Dictionary<(int, int), int> dp = new Dictionary<(int, int), int>();
+
+        int Backtrack(int i, int total)
+        {
+            if (i == nums.Length)
+            {
+                return total == target ? 1 : 0;
+            }
+
+            if (dp.ContainsKey((i, total)))
+            {
+                return dp[(i, total)];
+            }
+
+            dp[(i, total)] = Backtrack(i + 1, total + nums[i]) + Backtrack(i + 1, total - nums[i]);
+            return dp[(i, total)];
+        }
+
+        return Backtrack(0, 0);
+    }
+}
