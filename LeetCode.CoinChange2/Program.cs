1fe2e3bf0b0d95a8ef0a57e90ce4487e54226153
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-10-07 15:10:46 +0300

    to be continue

--- LeetCode.CoinChange2/Program.cs
+++ LeetCode.CoinChange2/Program.cs
@@ -1,32 +1,60 @@
 ﻿Solution solution = new();
-solution.Change(5, new[] { 1, 2, 5 });
+solution.Change4(5, new[] { 1, 2, 5 });
 
 
 Console.WriteLine("Hello, World!");
 
 
-public class Solution
+//Worked but time limit exceeded
+public partial class Solution
 {
     public int Change(int amount, int[] coins)
     {
-
-        void Backtrack(int total,int c)
+        int res = 0;
+        void Backtrack(int total, int last)
         {
-            if (total >= amount)
+            if (total > amount)
             {
                 return;
             }
-
-            total += c;
-
-            foreach (var coin in coins)
+            if (total == amount)
             {
-                Backtrack(total,coin);
+                res++;
+                return;
+            }
+            for (int i = last; i < coins.Length; i++)
+            {
+                Backtrack(total + coins[i], i);
             }
         }
-        Backtrack(0,0);
+        Backtrack(0, 0);
+        return res;
+    }
+}
 
 
-        return 0;
+//NeedCode solution
+
+public partial class Solution
+{
+    public int Change4(int amount, int[] coins)
+    {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = coins.Length - 1; i >= 0; i--)
+        {
+            int[] nextDP = new int[amount + 1];
+            nextDP[0] = 1;
+            for (int a = 1; a <= amount; a++)
+            {
+                nextDP[a] = dp[a];
+                if (a - coins[i] >= 0)
+                {
+                    nextDP[a] += nextDP[a - coins[i]];
+                }
+            }
+            dp = nextDP;
+        }
+        return dp[amount];
     }
 }
