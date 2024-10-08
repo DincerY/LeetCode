b2c795277dc4d91c26c4b6521f335a4a183249d8
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-10-08 09:15:47 +0300

    ..

--- LeetCode.CoinChange2/obj/Debug/net8.0/LeetCode.CoinChange2.AssemblyInfo.cs
+++ LeetCode.CoinChange2/obj/Debug/net8.0/LeetCode.CoinChange2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.CoinChange2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5024b9f74a695490225b119b79fd6408d803f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.CoinChange2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.CoinChange2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
