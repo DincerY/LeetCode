e7e4f83dd6f6a6971f71812a98e69c39f9de5681
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-10-15 09:59:24 +0300

    to be continue

---
+++ LeetCode.CombinationSum4/Program.cs
@@ -0,0 +1,45 @@
+﻿Solution solution = new();
+solution.CombinationSum4(new []{1,2,3},4);
+solution.CombinationSum4(new []{9},3);
+
+
+Console.WriteLine("Hello, World!");
+
+//Time Limit Exceeded
+public partial class Solution
+{
+    public int CombinationSum4(int[] nums, int target)
+    {
+        int res = 0;
+        void Backtrack(int value)   
+        {
+            if (value > target)
+            {
+                return;
+            }
+            
+            if (value == target)
+            {
+                res++;
+            }
+
+            foreach (var num in nums)
+            {
+                Backtrack(value+num);
+            }
+        }
+
+        Backtrack(0);
+        return res;
+    }
+}
+
+public partial class Solution
+{
+    public int CombinationSum4Dp(int[] nums, int target)
+    {
+        int res = 0;
+        
+        return res;
+    }
+}
