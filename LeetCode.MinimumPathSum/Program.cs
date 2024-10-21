24fb5ca5322ac29758df34444dc42273c0127d99
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-10-21 13:49:17 +0300

    to be continue

--- LeetCode.MinimumPathSum/Program.cs
+++ LeetCode.MinimumPathSum/Program.cs
@@ -1,5 +1,5 @@
 ﻿Solution solution = new();
-solution.MinPathSum(new[]
+solution.MinPathSum3(new[]
 {
     new[] { 1, 3, 1 },
     new[] { 1, 5, 1 },
@@ -35,6 +35,62 @@ public partial class Solution
             return memoization[r,c];
         }
         return Recursion(0,0);
+    }
+}
+
+
+public partial class Solution
+{
+    public int MinPathSum2(int[][] grid)
+    {
+        List<int> list = new();
+        void Recursion(int row,int col,int sum) 
+        {
+            if (row > grid.Length-1 || col > grid[0].Length-1)
+            {
+                return;
+            }
+            sum += grid[row][col];
+            if (row == grid.Length-1 && col == grid[0].Length-1)
+            {
+                list.Add(sum);
+            }
+            
+            Recursion(row+1,col, sum);
+            Recursion(row,col+1, sum);
+        }
+        Recursion(0,0,0);
+
+        return list.Min();
+    }
+}
+
+public partial class Solution
+{
+    public int MinPathSum3(int[][] grid)
+    {
+        int[,] dp = new int[grid.Length,grid[0].Length];
+        
+        for (int i = grid.Length-1; i >= 0; i--)
+        {
+            for (int j = grid[0].Length-1; j >= 0; j--)
+            {
+                dp[i, j] = grid[i][j];
+                if (i + 1 < grid.Length)
+                {
+                    dp[i, j] +=  dp[i + 1, j];
+                }
+
+                if (j + 1 < grid[0].Length)
+                {
+                    dp[i, j] +=  dp[i + 1, j];
+                }
+            }
+        }
+        
+        
+
+        return dp[0, 0];
     }
 }
 
