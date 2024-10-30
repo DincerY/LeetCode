aa3b160cad2153efe098f14f1f26cb17f16705d2
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-10-30 09:55:09 +0300

    ..

--- LeetCode.IntegerBreak/obj/Debug/net8.0/LeetCode.IntegerBreak.AssemblyInfo.cs
+++ LeetCode.IntegerBreak/obj/Debug/net8.0/LeetCode.IntegerBreak.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.IntegerBreak")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f0be14934861aba1688bb4776730c60ec569701")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.IntegerBreak")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.IntegerBreak")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
