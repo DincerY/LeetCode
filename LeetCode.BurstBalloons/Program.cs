258f37833d67bc24bf7e015386719d146ecaca67
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-11-12 10:22:07 +0300

    to be continue

--- LeetCode.BurstBalloons/Program.cs
+++ LeetCode.BurstBalloons/Program.cs
@@ -1,35 +1,66 @@
 ﻿Solution solution = new();
-solution.MaxCoins(new[] { 3, 1, 5, 8 });
+solution.MaxCoins2(new[] { 3, 1, 5, 8 });
 
 Console.WriteLine("Hello, World!");
 
-
-public class Solution
+//NeedCode's solution
+public partial class Solution
 {
     public int MaxCoins(int[] nums)
     {
-        int res = 0;
-        void Recursion(List<int> list)
-        {
-            int amount = 1;
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (i -1 >= 0)
-                {
-                    amount *= list[i - 1];
-                }
+        List<int> newNums = new List<int> { 1 };
+        newNums.AddRange(nums);
+        newNums.Add(1);
+        Dictionary<(int, int), int> dp = new Dictionary<(int, int), int>();
 
-                if (i+1 < list.Count)
-                {
-                    amount *= list[i + 1];
-                }
-                
-                amount *= list[i];
-                list.RemoveAt(i);
-                Recursion(list.ToList());
+        int Dfs(int l, int r)
+        {
+            if (l > r)
+            {
+                return 0;
+            }
+
+            if (dp.ContainsKey((l, r)))
+            {
+                return dp[(l, r)];
+            }
+
+            dp[(l, r)] = 0;
+            for (int i = l; i <= r; i++)
+            {
+                int coins = newNums[l - 1] * newNums[i] * newNums[r + 1];
+                coins += Dfs(l, i - 1) + Dfs(i + 1, r);
+                dp[(l, r)] = Math.Max(dp[(l, r)], coins);
+            }
+
+            return dp[(l, r)];
+        }
+
+        return Dfs(1, newNums.Count - 2);
+    }
+}
+
+
+public partial class Solution
+{
+    public int MaxCoins2(int[] nums)
+    {
+        List<int> newNums = new List<int> { 1 };
+        newNums.AddRange(nums);
+        newNums.Add(1);
+        void Recursion(List<int> tempNums,int first)
+        {
+            int total = 0;
+            for (int i = first; i < newNums.Count-1; i++)
+            {
+                total += tempNums[i-1] * tempNums[i] * tempNums[i+1];
+                var numList = tempNums.ToList();
+                numList.RemoveAt(i);
+                Recursion(numList,i + 1);
             }
         }
-        Recursion(new List<int>(nums));
+
+        Recursion(newNums,1);
         return 0;
     }
 }
--- LeetCode.BurstBalloons/obj/Debug/net8.0/LeetCode.BurstBalloons.AssemblyInfo.cs
+++ LeetCode.BurstBalloons/obj/Debug/net8.0/LeetCode.BurstBalloons.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.BurstBalloons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+588a837d4c94b2185aa31d9b919c727f69137a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343c0bc4aa5a2771c30d45b910a748d9ea769be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.BurstBalloons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.BurstBalloons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
