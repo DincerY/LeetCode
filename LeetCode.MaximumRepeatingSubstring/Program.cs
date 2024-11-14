a2ce0bc64f60431a47add51495c3679bd83120b3
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-11-14 09:53:38 +0300

    accepted

---
+++ LeetCode.MaximumRepeatingSubstring/Program.cs
@@ -0,0 +1,39 @@
+﻿Solution solution = new();
+solution.MaxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba");
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int MaxRepeating(string sequence, string word)
+    {
+        int count = 1;
+        while (true)
+        {
+            string newWord = "";
+            for (int i = 0; i < count; i++)
+            {
+                newWord = String.Concat(newWord,word);
+            }
+            if (!sequence.Contains(newWord))
+            {
+                return count - 1;
+            }
+            count++;
+        }
+    }
+    
+    
+    public int MaxRepeating2(string sequence, string word)
+    {
+        int count = 0;
+        String repeatedWord = word;
+        while (sequence.Contains(repeatedWord))
+        {
+            count++;
+            repeatedWord += word;
+        }
+        return count;
+    }
+}
