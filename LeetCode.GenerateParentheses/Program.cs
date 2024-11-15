00ee244d964baf1bbc1a2d6ccfe1d35baa552fb9
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-11-15 10:14:17 +0300

    to be continue

--- LeetCode.GenerateParentheses/Program.cs
+++ LeetCode.GenerateParentheses/Program.cs
@@ -1,53 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 
-namespace LeetCode.GenerateParentheses
+Solution solution = new();
+var a = solution.GenerateParenthesis(3);
+Console.WriteLine(a);
+
+public class Solution
 {
-    internal class Program
+    public IList<string> GenerateParenthesis(int n)
     {
-        public static void Main(string[] args)
-        {
-            Solution solution = new();
-            var a = solution.GenerateParenthesis(3);
-            Console.WriteLine(a);
-        }
-    }
-    
-    public class Solution {
-        public IList<string> GenerateParenthesis(int n)
-        {
-            Stack<string> stack = new();
-            List<string> res = new();
+        Stack<string> stack = new();
+        List<string> res = new();
 
-            void BackTrack(int openN, int closedN)
+        void BackTrack(int openN, int closedN)
+        {
+            if (openN == closedN && openN == n)
             {
-                if (openN == closedN && openN == n)
-                {
-                    res.Add(String.Join("",stack));
-                    return;
-                }
-
-                if (openN < n)
-                {
-                    stack.Push(")");  //(
-                    BackTrack(openN + 1,closedN);
-                    stack.Pop();
-                }
-                if (closedN < openN)
-                {
-                    stack.Push("(");  //)
-                    BackTrack(openN, closedN + 1);
-                    stack.Pop();
-                }
-                
+                res.Add(String.Join("", stack));
+                return;
             }
 
-            BackTrack(0,0);
-            return res;
+            if (openN < n)
+            {
+                stack.Push(")"); //(
+                BackTrack(openN + 1, closedN);
+                stack.Pop();
+            }
+
+            if (closedN < openN)
+            {
+                stack.Push("("); //)
+                BackTrack(openN, closedN + 1);
+                stack.Pop();
+            }
         }
+
+        BackTrack(0, 0);
+        return res;
     }
-    
 }
- 
