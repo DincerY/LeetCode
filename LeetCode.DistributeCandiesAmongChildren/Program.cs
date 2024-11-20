83d95de20f11f17e68ee9d440e2994ca8579ba66
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-11-20 09:44:33 +0300

    accepted

---
+++ LeetCode.DistributeCandiesAmongChildren/Program.cs
@@ -0,0 +1,29 @@
+﻿Solution solution = new();
+solution.DistributeCandies(5, 2);
+solution.DistributeCandies(3, 3);
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int DistributeCandies(int n, int limit)
+    {
+        int res = 0;
+        for (int i = 0; i <= limit; i++)
+        {
+            for (int j = 0; j <= limit; j++)
+            {
+                for (int k = 0; k <= limit; k++)
+                {
+                    if (i+k+j == n)
+                    {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
