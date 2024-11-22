eabb26f4005b629799a898e9a3fb593ee8fdf349
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-11-22 09:01:58 +0300

    to be continue

---
+++ LeetCode.NumberOfMusicPlaylists/Program.cs
@@ -0,0 +1,14 @@
+﻿Solution solution = new();
+solution.NumMusicPlaylists(3, 3, 1);
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int NumMusicPlaylists(int n, int goal, int k)
+    {
+        return 0;
+    }
+}
