f0157b9ed0a04b7316a39c7e7033ea8f1203e5a2
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-12-10 10:20:45 +0300

    to be continue

---
+++ LeetCode.PartitionArrayForMaximumSum/Program.cs
@@ -0,0 +1,14 @@
+﻿Solution solution = new();
+solution.MaxSumAfterPartitioning(new[] { 1, 15, 7, 9, 2, 5, 10 }, 3);
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int MaxSumAfterPartitioning(int[] arr, int k)
+    {
+        
+        
+        return 0;
+    }
+}
