775e3bd62c43df75ebaff9056298d66c8a2893c0
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-12-24 10:27:35 +0300

    accepted

--- LeetCode.ClimbingStairs/Program.cs
+++ LeetCode.ClimbingStairs/Program.cs
@@ -1,5 +1,5 @@
 ﻿Solution solution = new();
-solution.ClimbStairs2(5);
+solution.ClimbStairs4(5);
 
 
 Console.WriteLine("Hello, World!");
@@ -64,5 +64,63 @@ public partial class Solution {
             two = sum;
         }
         return two;
+    }
+}
+
+public partial class Solution {
+    public int ClimbStairs3(int n)
+    {
+        Dictionary<int, int> dp = new Dictionary<int, int>();
+        int Backtrack(int step)
+        {
+            if (step >= n)
+            {
+                return step == n ? 1 : 0;
+            }
+
+            if (dp.ContainsKey(step))
+            {
+                return dp[step];
+            }
+            
+            dp[step] = Backtrack(step+1) + Backtrack(step+2);
+            return dp[step];
+        }
+        return Backtrack(0);
+    }
+    
+    public int ClimbStairs4(int n)
+    {
+        int[] dp = new int[n + 1];
+        dp[^1] = 1;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (n - i >= 2)
+            {
+                dp[i] = dp[i + 1];
+                dp[i] += dp[i + 2];
+            }
+            else
+            {
+                dp[i] = dp[i + 1];
+            }
+        }
+
+        return dp[0];
+    }
+    
+    public int ClimbStairs5(int n)
+    {
+        int ptr1 = 1;
+        int ptr2 = 1;
+        for (int i = 0; i < n-1; i++)
+        {
+            int sum = ptr1 + ptr2;
+            ptr1 = ptr2;
+            ptr2 = sum;
+        }
+
+        return ptr2;
     }
 }
