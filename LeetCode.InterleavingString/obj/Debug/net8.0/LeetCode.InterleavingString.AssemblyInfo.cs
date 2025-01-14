174857581300e734897a44144a1530bbc35e57b7
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-01-14 10:34:47 +0300

    to be continue

--- LeetCode.InterleavingString/obj/Debug/net8.0/LeetCode.InterleavingString.AssemblyInfo.cs
+++ LeetCode.InterleavingString/obj/Debug/net8.0/LeetCode.InterleavingString.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.InterleavingString")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f7134fc44aae54139395d41eef564d121fa45f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.InterleavingString")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.InterleavingString")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
