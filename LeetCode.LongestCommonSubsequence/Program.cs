42bd0394dedc2f4811e144a4e72a43111ffd3385
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-01-21 10:43:38 +0300

    accepted

--- LeetCode.LongestCommonSubsequence/Program.cs
+++ LeetCode.LongestCommonSubsequence/Program.cs
@@ -1,10 +1,11 @@
 ﻿Solution solution = new();
-solution.LongestCommonSubsequence("abcde", "ace");
+solution.LongestCommonSubsequenceNoMemory("abcde", "ace");
+solution.LongestCommonSubsequenceNoMemory("bl", "yby");
 
 Console.WriteLine("Hello, World!");
 
 //NeedCode solution
-public class Solution
+public partial class Solution
 {
     public int LongestCommonSubsequence(string text1, string text2)
     {
@@ -26,5 +27,57 @@ public class Solution
         }
 
         return dp[0, 0];
+    }
+}
+
+
+
+public partial class Solution
+{
+    public int LongestCommonSubsequence2(string text1, string text2)
+    {
+        int[,] dp = new int[text2.Length+1,text1.Length+1];
+        for (int i = text2.Length-1; i >= 0 ; i--)
+        {
+            for (int j = text1.Length-1; j >= 0; j--)
+            {
+                if (text2[i] == text1[j])
+                {
+                    dp[i, j] += dp[i + 1, j + 1] + 1;
+                }
+                else
+                {
+                    dp[i, j] = Math.Max(dp[i+1,j],dp[i, j + 1]);
+                }
+            }
+        }
+        return dp[0,0];
+    }
+}
+
+
+public partial class Solution
+{
+    public int LongestCommonSubsequenceNoMemory(string text1, string text2)
+    {
+        int[] dp = new int[text1.Length+1];
+        for (int i = text2.Length-1; i >= 0 ; i--)
+        {
+            int prev = 0;
+            for (int j = text1.Length-1; j >= 0; j--)
+            {
+                int temp = dp[j];
+                if (text2[i] == text1[j])
+                {
+                    dp[j] = prev + 1;
+                }
+                else
+                {
+                    dp[j] = Math.Max(dp[j], dp[j+1]);
+                }
+                prev = temp;
+            }
+        }
+        return dp[0];
     }
 }
--- LeetCode.LongestIncreasingSubsequence/obj/Debug/net8.0/LeetCode.LongestIncreasingSubsequence.AssemblyInfo.cs
+++ LeetCode.LongestIncreasingSubsequence/obj/Debug/net8.0/LeetCode.LongestIncreasingSubsequence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.LongestIncreasingSubsequence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c654294c933229fc78d227b57acc3456db4844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.LongestIncreasingSubsequence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.LongestIncreasingSubsequence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
