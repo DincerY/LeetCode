5bd3f397c3c693d8959fc4b71960b9c48858eadb
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-01-31 10:55:40 +0300

    to be continue

--- LeetCode.PerfectSquares/Program.cs
+++ LeetCode.PerfectSquares/Program.cs
@@ -1,13 +1,15 @@
 ﻿Solution solution = new();
-solution.NumSquares(37);
-solution.NumSquares(12);
-solution.NumSquares(13);
+solution.NumSquaresBruteForce(12);
+solution.NumSquaresBruteForce(13);
+solution.NumSquaresBruteForce(37);
+solution.NumSquaresBruteForce(55);
+
 
 
 Console.WriteLine("Hello, World!");
 
 //NeedCode solution
- public class Solution {
+ public partial class Solution {
      public int NumSquares(int n) {
          int[] dp = new int[n + 1];
          for (int i = 1; i <= n; i++) {
@@ -22,6 +24,125 @@ Console.WriteLine("Hello, World!");
              }
          }
          return dp[n];
+     }
+ }
+
+
+
+ public partial class Solution {
+     public int NumSquaresBruteForce(int n)
+     {
+         int min = 10000;
+         void Backtrack(int total,int val)
+         {
+             if (total == n)
+             {
+                 min = Math.Min(min, val);
+                 return;
+             }
+             if (total > n)
+             {
+                 return;
+             }
+             for (int i = 1; i*i <= n; i++)
+             {
+                 if ((n-total) - i*i < 0)
+                 {
+                     break;
+                 }
+                 Backtrack(total + (i*i) ,val+1);
+             }
+         } 
+         Backtrack(0,0);
+         return min;
+     }
+ }
+
+//Dynamic Programming memoization
+ public partial class Solution {
+     public int NumSquaresBrute2(int n)
+     {
+         int[] dp = new int[n+1];
+         dp[0] = 0;
+         int min = 10000;
+         int Backtrack(int total,int val)
+         {
+             if (total == n)
+             {
+                 min = Math.Min(min, val);
+                 return min;
+             }
+             if (total > n)
+             {
+                 return 0;
+             }
+
+             int tempMin = 100000;
+             for (int i = 1; i*i <= n; i++)
+             {
+                 if ((n-total) - i*i < 0)
+                 {
+                     break;
+                 }
+                 tempMin = Math.Min(tempMin,Backtrack(total + (i*i) ,val+1));
+             }
+
+             return 0;
+         } 
+         Backtrack(0,0);
+         return min;
+     }
+ }
+
+ public partial class Solution {
+     public int NumSquares3(int n)
+     {
+         int[] arr = new int[n+1];
+         List<int> list = new();
+         for (int i = 1; i*i<= n; i++)
+         {
+             list.Add(i*i);
+         }
+         for (int i = 1; i < n+1; i++)
+         {
+             arr[i] = 100000;
+             foreach (var val in list)
+             {
+                 if (i - val >= 0)
+                 {
+                     arr[i] = Math.Min(arr[i], arr[i - val] + 1);
+                 }
+                 else
+                 {
+                     break;
+                 }
+             }
+         }
+         return arr[n];
+     }
+ }
+ 
+ public partial class Solution {
+     public int NumSquares4(int n)
+     {
+         int[] arr = new int[n+1];
+         for (int i = 1; i < n+1; i++)
+         {
+             arr[i] = 100000;
+             for (int j = 1; j*j<= n; j++)
+             {
+                 int sq = j * j;
+                 if (i - sq >= 0)
+                 {
+                     arr[i] = Math.Min(arr[i], arr[i - sq] + 1);
+                 }
+                 else
+                 {
+                     break;
+                 }
+             }
+         }
+         return arr[n];
      }
  }
  
