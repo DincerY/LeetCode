c53897e1f08121bfb65f24675bf9460416aa2f7d
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-02-21 09:56:02 +0300

    accepted

--- LeetCode.MinCostClimbingStairs/Program.cs
+++ LeetCode.MinCostClimbingStairs/Program.cs
@@ -1,5 +1,5 @@
 ﻿Solution solution = new();
-solution.MinCostClimbingStairs3(new[]
+solution.MinCostClimbingStairs4(new[]
 {
     10, 15, 20
 });
@@ -27,7 +27,7 @@ public partial class Solution
     }
 }
 
-//Bu kod tam bitmedi (devam edicem)
+
 public partial class Solution
 {
     public int MinCostClimbingStairs2(int[] cost)
@@ -35,25 +35,26 @@ public partial class Solution
         int min = 10000;
         void Backtrack(int stair, int total)
         {
-            if (stair >= cost.Length)
+            if (stair >= cost.Length-1)
             {
                 min = Math.Min(min, total);
                 return;
             }
-            Backtrack(stair+1,total+cost[stair]);
-            var temp = 0;
-            if (stair+2 < cost.Length)
+            Backtrack(stair+1,total+cost[stair+1]);
+            int temp = 0;
+            if (stair + 2 < cost.Length)
             {
                 temp = total + cost[stair + 2];
             }
-            Backtrack(stair+2,temp==0?total:temp);
+            Backtrack(stair+2,temp == 0 ? total : temp);
         }
-        Backtrack(0,0);
-        return 0;
+        Backtrack(0,cost[0]);
+        Backtrack(1,cost[1]);
+        return min;
     }
 }
 
-//bunu bir array yerine sadece iki tane değişken ile yapabiliriz (devam edicem)
+
 public partial class Solution
 {
     public int MinCostClimbingStairs3(int[] cost)
@@ -65,5 +66,21 @@ public partial class Solution
             dp[i] = min + cost[i-1];
         }
         return Math.Min(dp[1], dp[2]);
+    }
+}
+
+public partial class Solution
+{
+    public int MinCostClimbingStairs4(int[] cost)
+    {
+        int firstBlock = 0;
+        int secondBlock = 0;
+        for (int i = cost.Length-1; i >= 0; i--)
+        {
+            int min = Math.Min(firstBlock, secondBlock);
+            secondBlock = firstBlock;
+            firstBlock = min + cost[i];
+        }
+        return Math.Min(firstBlock, secondBlock);
     }
 }
--- LeetCode.MinCostClimbingStairs/obj/Debug/net8.0/LeetCode.MinCostClimbingStairs.AssemblyInfo.cs
+++ LeetCode.MinCostClimbingStairs/obj/Debug/net8.0/LeetCode.MinCostClimbingStairs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.MinCostClimbingStairs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d55d9ca08535691037eb9c36b2b498adab6dcb2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eccd333a589e8382b9f9c97e81d575a98f97487")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.MinCostClimbingStairs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.MinCostClimbingStairs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
