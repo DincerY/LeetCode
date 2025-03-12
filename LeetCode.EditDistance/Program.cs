f1d2e647145fa21ade8da8d607866883f669a3d3
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-03-12 10:35:26 +0300

    to be continue

--- LeetCode.EditDistance/Program.cs
+++ LeetCode.EditDistance/Program.cs
@@ -1,10 +1,8 @@
 ﻿Solution solution = new();
-solution.MinDistance2("horse", "ros");
-solution.MinDistance2("intention", "execution");
-
+solution.MinDistance3("horse", "ros");
+solution.MinDistance3("intention", "execution");
 
 Console.WriteLine("Hello, World!");
-
 
 //NeedCode's solution
 public partial class Solution
@@ -40,8 +38,6 @@ public partial class Solution
     }
 }
 
-
-
 public partial class Solution
 {
     public int MinDistance2(string word1, string word2)
@@ -67,6 +63,42 @@ public partial class Solution
                 Backtrack(i,j+1,val+1); //Insert
                 Backtrack(i+1,j+1,val+1); //Replace
                 Backtrack(i+1,j,val+1); //Delete
+            }
+        }
+        Backtrack(0,0,0);
+        return min;
+    }
+}
+
+
+public partial class Solution
+{
+    public int MinDistance3(string word1, string word2)
+    {
+        int min = int.MaxValue;
+        void Backtrack(int word1Index,int word2Index,int op)
+        {
+            if (word1Index >= word1.Length || word2Index >= word2.Length)
+            {
+                if (word2Index >= word2.Length)
+                {
+                    min += word1Index - word2Index;
+                }
+                if (word1Index >= word1.Length && word2Index >= word2.Length)
+                {
+                    min = Math.Min(min, op);
+                }
+                return;
+            }
+            if (word1[word1Index] == word2[word2Index])
+            {
+                Backtrack(word1Index+1,word2Index+1,op);
+            }
+            else
+            {
+                Backtrack(word1Index,word2Index+1,op+1); //Insert
+                Backtrack(word1Index+1,word2Index,op+1); //Delete
+                Backtrack(word1Index+1,word2Index+1,op+1); //Replace
             }
         }
         Backtrack(0,0,0);
