2c10d088a7dee418c0b3147bd74683b2fd2e0ea0
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-04-07 09:40:04 +0300

    to be continue

---
+++ LeetCode.BitwiseAndOfNumbersRange/Program.cs
@@ -0,0 +1,20 @@
+﻿Solution solution = new();
+solution.RangeBitwiseAnd(5,7);
+solution.RangeBitwiseAnd(0,0);
+solution.RangeBitwiseAnd(1,2147483647);
+
+Console.WriteLine("Hello, World!");
+
+
+
+public partial class Solution {
+    public int RangeBitwiseAnd(int left, int right)
+    {
+        int res = left;
+        for (int i = left+1; i <= right; i++)
+        {
+            res &= i;
+        }
+        return res;
+    }
+}
