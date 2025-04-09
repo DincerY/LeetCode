9916530946db49349495ce522f7efed3a2507149
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-04-09 10:23:17 +0300

    accepted

---
+++ LeetCode.HappyNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿Solution solution = new();
+solution.IsHappy(19);
+solution.IsHappy(2);
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public bool IsHappy(int n)
+    {
+        HashSet<int> set = new HashSet<int>();
+        while (n != 1)
+        {
+            int temp = n;
+            n = 0;
+            while (temp > 0)
+            {
+                n += (int)Math.Pow(temp % 10,2);
+                temp /= 10;
+            }
+
+            if (set.Contains(n))
+            {
+                return false;
+            }
+            else
+            {
+                set.Add(n);
+            }
+        }
+        return true;
+    }
+}
