5b5f0f93edf39ac00749336cbe6978a514d592ea
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-04-14 10:51:47 +0300

    to be continue

---
+++ LeetCode.CourseSchedule/Program.cs
@@ -0,0 +1,23 @@
+﻿Solution solution = new();
+solution.CanFinish(2, new[]
+{
+    new[] { 1, 0 }
+});
+
+solution.CanFinish(2, new[]
+{
+    new[] { 1, 0 },
+    new[] { 0, 1 },
+});
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public bool CanFinish(int numCourses, int[][] prerequisites)
+    {
+        
+        return true;
+    }
+}
