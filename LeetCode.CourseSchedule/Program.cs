7e0490c0010ed52dcd35a3da5fa1fb4f90c02167
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-04-18 09:57:25 +0300

    accepted

--- LeetCode.CourseSchedule/Program.cs
+++ LeetCode.CourseSchedule/Program.cs
@@ -10,16 +10,15 @@ solution.CanFinish(2, new[]
     new[] { 0, 1 },
 });*/
 
-solution.CanFinish(2, new[]
+solution.CanFinish2(5, new[]
 {
     new[] { 0, 1 },
     new[] { 0, 2 },
     new[] { 1, 3 },
     new[] { 1, 4 },
     new[] { 3, 4 },
-
 });
-solution.CanFinish(2, new[]
+solution.CanFinish2(3, new[]
 {
     new[] { 0, 1 },
     new[] { 1, 2 },
@@ -42,14 +41,14 @@ public partial class Solution
         {
             if (!preMap.ContainsKey(prerequisite[0]))
             {
-                preMap.Add(prerequisite[0],new List<int>(){prerequisite[1]});
+                preMap.Add(prerequisite[0], new List<int>() { prerequisite[1] });
             }
             else
             {
                 preMap[prerequisite[0]].Add(prerequisite[1]);
             }
         }
-        
+
         HashSet<int> visitSet = new();
         bool res = true;
         void Dfs(int nodeVal)
@@ -64,7 +63,7 @@ public partial class Solution
             {
                 return;
             }
-            
+
             visitSet.Add(nodeVal);
             foreach (var i in preMap[nodeVal])
             {
@@ -72,39 +71,42 @@ public partial class Solution
             }
         }
         Dfs(preMap.Keys.FirstOrDefault());
-        
         return res;
     }
 }
 
 //NeedCode solution
-public partial class Solution {
-    public bool CanFinish2(int numCourses, int[][] prerequisites) {
+public partial class Solution
+{
+    public bool CanFinish2(int numCourses, int[][] prerequisites)
+    {
         Dictionary<int, List<int>> preMap = new Dictionary<int, List<int>>();
-
-        for (int i = 0; i < numCourses; i++) {
+        for (int i = 0; i < numCourses; i++)
+        {
             preMap[i] = new List<int>();
         }
-
-        foreach (var pair in prerequisites) {
+        foreach (var pair in prerequisites)
+        {
             int crs = pair[0];
             int pre = pair[1];
             preMap[crs].Add(pre);
         }
-
         HashSet<int> visiting = new HashSet<int>();
-
-        bool Dfs(int crs) {
-            if (visiting.Contains(crs)) {
+        bool Dfs(int crs)
+        {
+            if (visiting.Contains(crs))
+            {
                 return false;
             }
-            if (preMap[crs].Count == 0) {
+            if (preMap[crs].Count == 0)
+            {
                 return true;
             }
-
             visiting.Add(crs);
-            foreach (var pre in preMap[crs]) {
-                if (!Dfs(pre)) {
+            foreach (var pre in preMap[crs])
+            {
+                if (!Dfs(pre))
+                {
                     return false;
                 }
             }
@@ -112,9 +114,10 @@ public partial class Solution {
             preMap[crs].Clear();
             return true;
         }
-
-        for (int c = 0; c < numCourses; c++) {
-            if (!Dfs(c)) {
+        for (int c = 0; c < numCourses; c++)
+        {
+            if (!Dfs(c))
+            {
                 return false;
             }
         }
@@ -131,7 +134,7 @@ public class Graph
     {
         foreach (var prerequisite in prerequisites)
         {
-            AddEdge(prerequisite[0],prerequisite[1]);
+            AddEdge(prerequisite[0], prerequisite[1]);
         }
     }
 
@@ -163,7 +166,7 @@ public class Graph
             Console.WriteLine("");
         }
         Console.WriteLine("Printing Finished");
-
+        
     }
     public void Dfs(int vertex, int count)
     {
@@ -173,7 +176,7 @@ public class Graph
         }
         if (!adjList.ContainsKey(vertex))
         {
-             return;   
+            return;
         }
 
         if (adjList[vertex].Count <= 0)
@@ -184,9 +187,8 @@ public class Graph
 
         Console.WriteLine($"vertex : {vertex}");
         foreach (var vrtx in adjList[vertex])
-        { 
-            Dfs(vrtx,count+1);
+        {
+            Dfs(vrtx, count + 1);
         }
     }
-    
 }
