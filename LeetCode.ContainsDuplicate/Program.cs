c9aa1ed43402a75d7ea869900705610ec2d9a27f
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-04-22 10:56:26 +0300

    accepted

---
+++ LeetCode.ContainsDuplicate/Program.cs
@@ -0,0 +1,23 @@
+﻿Solution solution = new();
+solution.ContainsDuplicate(new []{1,2,3,1});
+
+Console.WriteLine("Hello, World!");
+
+public class Solution {
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> hash = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (hash.Contains(nums[i]))
+            {
+                return true;
+            }
+            else
+            {
+                hash.Add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
