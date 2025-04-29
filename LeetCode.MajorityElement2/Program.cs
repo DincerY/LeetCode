997f274a08a2c63d8d6c609741f11d4e76bdeccd
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-04-29 10:48:45 +0300

    accepted

---
+++ LeetCode.MajorityElement2/Program.cs
@@ -0,0 +1,79 @@
+﻿Solution solution = new();
+solution.MajorityElement2(new[] { 2,2,1,3 });
+
+solution.MajorityElement2(new[] { 3, 2, 3 });
+solution.MajorityElement2(new[] { 1,1,2,2,2,2,3,3,3,3,3,3});
+solution.MajorityElement2(new[] { 1 });
+solution.MajorityElement2(new[] { 1, 2 });
+
+Console.WriteLine("Hello, World!");
+
+
+public partial class Solution
+{
+    public IList<int> MajorityElement(int[] nums)
+    {
+        List<int> res = new();
+        Dictionary<int, int> dic = new();
+        foreach (var num in nums)
+        {
+            if (dic.ContainsKey(num))
+            {
+                dic[num]++;
+            }
+            else
+            {
+                dic.Add(num,1);
+            }
+            if (!res.Contains(num) && dic[num] > nums.Length / 3)
+            {
+                res.Add(num);   
+            }
+        }
+        return res;
+    }
+}
+
+public partial class Solution
+{
+    public IList<int> MajorityElement2(int[] nums)
+    {
+        var count = new Dictionary<int, int>();
+        
+        foreach (var n in nums)
+        {
+            if (count.ContainsKey(n))
+            {
+                count[n]++;
+            }
+            else
+            {
+                count[n] = 1;
+            }
+            
+            if (count.Count <= 2)
+            {
+                continue;
+            }
+            var newCount = new Dictionary<int, int>();
+            foreach (var kvp in count)
+            {
+                if (kvp.Value > 1)
+                {
+                    newCount[kvp.Key] = kvp.Value - 1;
+                }
+            }
+            count = newCount;
+        }
+        
+        var res = new List<int>();
+        foreach (var n in count.Keys)
+        {
+            if (nums.Count(x => x == n) > nums.Length / 3)
+            {
+                res.Add(n);
+            }
+        }
+        return res;
+    }
+}
--- LeetCode.MajorityElement2/obj/Debug/net8.0/LeetCode.MajorityElement2.AssemblyInfo.cs
+++ LeetCode.MajorityElement2/obj/Debug/net8.0/LeetCode.MajorityElement2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.RectangleArea")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.MajorityElement2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.RectangleArea")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.RectangleArea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf1745b446ad8482a5e23e92d68b7a08befef85")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.MajorityElement2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.MajorityElement2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
