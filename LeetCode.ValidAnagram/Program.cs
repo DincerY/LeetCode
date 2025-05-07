a9968ba84247a22a61824e16d21560d05477bb18
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-05-07 10:23:13 +0300

    to be continue

---
+++ LeetCode.ValidAnagram/Program.cs
@@ -0,0 +1,40 @@
+﻿Solution solution = new();
+solution.IsAnagram("anagram","nagaram");
+solution.IsAnagram("rat","car");
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution {
+    public bool IsAnagram(string s, string t)
+    {
+        Dictionary<char, int> dic = new();
+        foreach (var chr in s)
+        {
+            if (dic.ContainsKey(chr))
+            {
+                dic[chr]++;
+            }
+            else
+            {
+                dic.Add(chr,1);
+            }
+        }
+
+        foreach (var chr in t)
+        {
+            if (dic.ContainsKey(chr))
+            {
+                if (dic[chr] != 0)
+                {
+                    dic[chr]--;
+                }
+                if (dic[chr] == 0)
+                {
+                    dic.Remove(chr);
+                }
+            }
+        }
+        return dic.Count == 0;
+    }
+}
