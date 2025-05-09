e0cdea3d37affb503ca7465a63cfbcad10b0444c
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-05-09 10:46:17 +0300

    accepted

--- LeetCode.CloneGraph/Program.cs
+++ LeetCode.CloneGraph/Program.cs
@@ -1,9 +1,19 @@
 ﻿Solution solution = new Solution();
-solution.CloneGraph(new Node(1, new List<Node>(new[] { new Node(2), new Node(4) })));
-
+var node1 = new Node(1, new List<Node>());
+var node2 = new Node(2, new List<Node>());
+var node3 = new Node(3, new List<Node>());
+var node4 = new Node(4, new List<Node>());
+node1.neighbors.Add(node2);
+node1.neighbors.Add(node4);
+node2.neighbors.Add(node1);
+node2.neighbors.Add(node3);
+node3.neighbors.Add(node2);
+node3.neighbors.Add(node4);
+node4.neighbors.Add(node1);
+node4.neighbors.Add(node3);
+solution.CloneGraph(node1);
 
 Console.WriteLine("Hello, World!");
-
 
 public class Node
 {
@@ -29,11 +39,30 @@ public class Node
     }
 }
 
-
-public class Solution
+//NeedCode solution
+public partial class Solution
 {
     public Node CloneGraph(Node node)
     {
-        return null;
+        Dictionary<Node, Node> oldToNew = new Dictionary<Node, Node>();
+
+        Node Dfs(Node n)
+        {
+            if (oldToNew.ContainsKey(n))
+            {
+                return oldToNew[n];
+            }
+
+            Node copy = new Node(n.val);
+            oldToNew[n] = copy;
+            foreach (var nei in n.neighbors)
+            {
+                copy.neighbors.Add(Dfs(nei));
+            }
+
+            return copy;
+        }
+
+        return node != null ? Dfs(node) : null;
     }
 }
