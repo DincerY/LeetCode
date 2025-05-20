18d5dd7677222bc8afbe5528b02fcff60c15f69d
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-05-20 10:05:15 +0300

    accepted

---
+++ LeetCode.LongestRepeatingCharacterReplacement/Program.cs
@@ -0,0 +1,74 @@
+﻿Solution solution = new();
+solution.CharacterReplacement("ABAB", 2);
+solution.CharacterReplacement("AABABBA", 1);
+
+Console.WriteLine("Hello, World!");
+
+
+public partial class Solution {
+    public int CharacterReplacement(string s, int k) {
+        Dictionary<char, int> count = new Dictionary<char, int>();
+        
+        int l = 0;
+        int maxf = 0;
+        int res = 0;
+        for (int r = 0; r < s.Length; r++) {
+            if (count.ContainsKey(s[r])) {
+                count[s[r]]++;
+            } else {
+                count[s[r]] = 1;
+            }
+            maxf = Math.Max(maxf, count[s[r]]);
+
+            if ((r - l + 1) - maxf > k) {
+                count[s[l]]--;
+                if (count[s[l]] == 0) {
+                    count.Remove(s[l]);
+                }
+                l++;
+            }
+
+            res = Math.Max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
+
+
+public partial class Solution {
+    public int CharacterReplacement2(string s, int k) {
+        Dictionary<char, int> count = new Dictionary<char, int>();
+        int res = 0;
+        int l = 0;
+
+        for (int r = 0; r < s.Length; r++) {
+            if (count.ContainsKey(s[r])) {
+                count[s[r]]++;
+            } else {
+                count[s[r]] = 1;
+            }
+
+            while ((r - l + 1) - Max(count.Values) > k) {
+                count[s[l]]--;
+                if (count[s[l]] == 0) {
+                    count.Remove(s[l]);
+                }
+                l++;
+            }
+            res = Math.Max(res, r - l + 1);
+        }
+
+        return res;
+    }
+
+    private int Max(IEnumerable<int> values) {
+        int max = int.MinValue;
+        foreach (var value in values) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+}
