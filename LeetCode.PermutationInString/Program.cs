edb7c1f480729c9808ea50f893cd8b8463f5f5f8
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-06-30 10:51:51 +0300

    accepted

---
+++ LeetCode.PermutationInString/Program.cs
@@ -0,0 +1,113 @@
+﻿Solution solution = new();
+solution.CheckInclusion2("ab", "eidboaoo");
+solution.CheckInclusion2("adc", "dcda");
+
+Console.WriteLine("Hello, World!");
+
+//Time limit exceeded
+public partial class Solution
+{
+    public bool CheckInclusion(string s1, string s2)
+    {
+        Dictionary<char, int> dic = new();
+        for (int i = 0; i < s1.Length; i++)
+        {
+            dic[s1[i]] = 1 + (dic.ContainsKey(s1[i]) ? dic[s1[i]] : 0);
+        }
+
+        int r = 0;
+        for (int l = 0; l < s2.Length; l++)
+        {
+            if (dic.ContainsKey(s2[l]))
+            {
+                r = l;
+                while (r < s2.Length && dic.ContainsKey(s2[r]))
+                {
+                    dic[s2[r]]--;
+                    if (dic[s2[r]] == 0)
+                    {
+                        dic.Remove(s2[r]);
+                    }
+                    r++;
+                }
+            }
+
+            if (dic.Count == 0)
+            {
+                return true;
+            }
+
+            var tempL = l;
+            while (r - tempL > 0)
+            {
+                dic[s2[tempL]] = 1 + (dic.ContainsKey(s2[tempL]) ? dic[s2[tempL]] : 0);
+                tempL++;
+            }
+        }
+
+        return false;
+    }
+}
+
+public partial class Solution
+{
+    public bool CheckInclusion2(string s1, string s2)
+    {
+        if (s1.Length > s2.Length)
+        {
+            return false;
+        }
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        for (int i = 0; i < s1.Length; i++)
+        {
+            s1Count[s1[i] - 'a']++;
+            s2Count[s2[i] - 'a']++;
+        }
+
+        int matches = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            if (s1Count[i] == s2Count[i])
+            {
+                matches++;
+            }
+        }
+
+        int l = 0;
+        for (int r = s1.Length; r < s2.Length; r++)
+        {
+            if (matches == 26)
+            {
+                return true;
+            }
+
+            int index = s2[r] - 'a';
+            s2Count[index]++;
+            if (s1Count[index] == s2Count[index])
+            {
+                matches++;
+            }
+            else if (s1Count[index] + 1 == s2Count[index])
+            {
+                matches--;
+            }
+
+            index = s2[l] - 'a';
+            s2Count[index]--;
+            if (s1Count[index] == s2Count[index])
+            {
+                matches++;
+            }
+            else if (s1Count[index] - 1 == s2Count[index])
+            {
+                matches--;
+            }
+
+            l++;
+        }
+
+        return matches == 26;
+    }
+}
--- LeetCode.PermutationInString/obj/Debug/net8.0/LeetCode.PermutationInString.AssemblyInfo.cs
+++ LeetCode.PermutationInString/obj/Debug/net8.0/LeetCode.PermutationInString.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.LongestRepeatingCharacterReplacement")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.PermutationInString")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.LongestRepeatingCharacterReplacement")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.LongestRepeatingCharacterReplacement")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58cfc6278dc2cc8349659e751a5b6a2f2335a998")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.PermutationInString")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.PermutationInString")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
