e7c4c1c4568107aac02e9335d6013c81200ee582
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-08-20 10:19:16 +0300

    accepted

---
+++ LeetCode.MergeStringsAlternately/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+Solution solution = new();
+solution.MergeAlternately("abc", "pqr");
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        StringBuilder builder = new();
+        int word1Pointer = 1;
+        builder.Append(word1[0]);
+        int word2Pointer = 0;
+        
+        int len1 = word1.Length;
+        int len2 = word2.Length;
+
+        while (word1Pointer < len1 && word2Pointer < len2)
+        {
+            if (word1Pointer > word2Pointer)
+            {
+                builder.Append(word2[word2Pointer]);
+                word2Pointer++;
+            }
+            else
+            {
+                builder.Append(word1[word1Pointer]);
+                word1Pointer++;
+            }
+        }
+
+        while (word1Pointer < len1)
+        {
+            builder.Append(word1[word1Pointer]);
+            word1Pointer++;
+        }
+        while (word2Pointer < len2)
+        {
+            builder.Append(word2[word2Pointer]);
+            word2Pointer++;
+        }
+
+        return builder.ToString();
+    }
+}
--- LeetCode.MergeStringsAlternately/obj/Debug/net8.0/LeetCode.MergeStringsAlternately.AssemblyInfo.cs
+++ LeetCode.MergeStringsAlternately/obj/Debug/net8.0/LeetCode.MergeStringsAlternately.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.PermutationInString")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.MergeStringsAlternately")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.PermutationInString")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.PermutationInString")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb7c1f480729c9808ea50f893cd8b8463f5f5f8")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.MergeStringsAlternately")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.MergeStringsAlternately")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
