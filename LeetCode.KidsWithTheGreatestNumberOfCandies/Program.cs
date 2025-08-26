f65545f20961bd70d75f8c7f7dc33bf07550dda8
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-08-26 10:35:48 +0300

    accepted

---
+++ LeetCode.KidsWithTheGreatestNumberOfCandies/Program.cs
@@ -0,0 +1,28 @@
+﻿Solution solution = new();
+solution.KidsWithCandies(new[] { 2, 3, 5, 1, 3 }, 3);
+solution.KidsWithCandies(new[] { 4, 2, 1, 1, 2 }, 1);
+
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        List<bool> res = new();
+        int max = candies.Max();
+
+        foreach (var candy in candies)
+        {
+            if (candy + extraCandies >= max)
+            {
+                res.Add(true);
+            }
+            else
+            {
+                res.Add(false);
+            }
+        }
+        return res;
+    }
+}
--- LeetCode.KidsWithTheGreatestNumberOfCandies/obj/Debug/net8.0/LeetCode.KidsWithTheGreatestNumberOfCandies.AssemblyInfo.cs
+++ LeetCode.KidsWithTheGreatestNumberOfCandies/obj/Debug/net8.0/LeetCode.KidsWithTheGreatestNumberOfCandies.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.GreatestCommonDivisorOfStrings")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.KidsWithTheGreatestNumberOfCandies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.GreatestCommonDivisorOfStrings")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.GreatestCommonDivisorOfStrings")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.KidsWithTheGreatestNumberOfCandies")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.KidsWithTheGreatestNumberOfCandies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
