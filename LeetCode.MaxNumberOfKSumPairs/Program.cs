09f35fb891c894e1e7a346d885d4d8b0c949caec
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-09-22 09:40:05 +0300

    accepted

---
+++ LeetCode.MaxNumberOfKSumPairs/Program.cs
@@ -0,0 +1,35 @@
+﻿Solution solution = new();
+solution.MaxOperations(new[] { 1, 2, 3, 4 }, 5);
+solution.MaxOperations(new[] { 3, 1, 3, 4, 3 }, 6);
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int MaxOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        int left = 0;
+        int right = nums.Length - 1;
+        int res = 0;
+        while (left < right)
+        {
+            int total = nums[left] + nums[right];
+            if (total == k)
+            {
+                left++;
+                right--;
+                res++;
+            }
+            else if (total < k)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return res;
+    }
+}
--- LeetCode.MaxNumberOfKSumPairs/obj/Debug/net8.0/LeetCode.MaxNumberOfKSumPairs.AssemblyInfo.cs
+++ LeetCode.MaxNumberOfKSumPairs/obj/Debug/net8.0/LeetCode.MaxNumberOfKSumPairs.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.IsSubsequence")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.MaxNumberOfKSumPairs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.IsSubsequence")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.IsSubsequence")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c629068a52aae667fc13b50c42aaaba49da61c56")]
+[assembly: System.Reflection.AssemblyProductAttribute("LeetCode.MaxNumberOfKSumPairs")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.MaxNumberOfKSumPairs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
