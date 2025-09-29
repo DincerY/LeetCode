cae4a2c95005ac1ff45e3b1f0f814240f795bc80
Dinçer Yiğit <dinceryigit60@gmail.com>
2025-09-29 10:03:39 +0300

    to be continue

---
+++ LeetCode.MaxConsecutiveOnesIII/Program.cs
@@ -0,0 +1,51 @@
+﻿Solution solution = new();
+solution.LongestOnes(new[] { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }, 2);
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        //I should compare curRes and maxRes
+        int res = 0;
+        int l = 0;
+        int r = 0;
+        int temp = k;
+        while (l <= r)
+        {
+            if (nums[r] == 0)
+            {
+                if (temp > 0)
+                {
+                    temp--;
+                    res++;
+                    r++;
+                }
+                else
+                {
+                    if (nums[l] == 1)
+                    {
+                        res--;
+                        l++;
+                    }
+                    else
+                    {
+                        res--;
+                        l++;
+                        temp++;
+                    }
+                }
+            }
+            else
+            {
+                res++;
+                r++;
+            }
+            
+        }
+
+
+        return 0;
+    }
+}
